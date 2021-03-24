6dbe1c6bf393a9a59dddbe5da2a66747c7963806
Jack Hubert <jphubert93@gmail.com>
2021-03-24 12:10:37 -0600

    updated execute program & environment functions

--- environment.py
+++ environment.py
@@ -1,30 +1,7 @@
 from driver import chrome_webdriver
-from execute import execute_script
-from import_list import generate_document_list
-from login import account_login
-from variables import download_documents, file_name, sheet_name, target_directory, county
-from user_prompts import continue_prompt, request_more_information
-from export import export_document
+from execute import execute_program
 
-def create_abstraction():
-    document_list = generate_document_list(target_directory, file_name, sheet_name)
-    browser = chrome_webdriver(target_directory)
-    account_login(browser)
-    abstract_dict = execute_script(browser, target_directory, document_list, download_documents)
-    export_document(target_directory, file_name, abstract_dict)
-    if continue_prompt(target_directory, file_name, sheet_name):
-        document_list = request_more_information(target_directory, file_name, sheet_name)
+from variables import download, file_name, sheet_name, target_directory, county
 
-
-def test():
-    abstract_dict["Grantor"].append("Test")
-    abstract_dict["Grantee"].append("Test")
-    abstract_dict["Book"].append("Test")
-    abstract_dict["Page"].append("Test")
-    abstract_dict["Reception Number"].append("Test")
-    abstract_dict["Document Type"].append("Test")
-    abstract_dict["Recording Date"].append("Test")
-    abstract_dict["Legal"].append("Test")
-    abstract_dict["Related Documents"].append("Test")
-    abstract_dict["Comments"].append("Test")
-    return abstract_dict
+browser = chrome_webdriver(target_directory)
+execute_program(browser, target_directory, file_name, sheet_name, download)
--- execute.py
+++ execute.py
@@ -4,6 +4,10 @@ from open import open_document
 from record import record_bad_search, record_document
 from search import document_number_search
 from file_management import create_folder
+from login import account_login
+from user_prompts import continue_prompt, request_more_information
+from import_list import generate_document_list
+from export import export_document
 
 import os
 
@@ -18,12 +22,12 @@ def remaining_downloads(document_list, document_number):
     return len(document_list) - document_list.index(document_number) - 1
 
 
-def search_documents_from_list(browser, document_list, download_documents):
+def search_documents_from_list(browser, document_list):
     for document_number in document_list:
         document_number_search(browser, document_number)
         if open_document(browser, document_number):
             record_document(browser, abstract_dict, document_number)
-            if download_documents:
+            if download:
                 download_document(browser)
             print(f'Document located at reception number {document_number} recorded, \
                 {remaining_downloads(document_list, document_number)} documents remaining.')
@@ -33,8 +37,23 @@ def search_documents_from_list(browser, document_list, download_documents):
                 {remaining_downloads(document_list, document_number)} documents remaining.')
 
 
-def execute_script(browser, target_directory, document_list, download_documents):
-    if download_documents:
+def create_abstraction_dictionary(browser, target_directory, document_list, download):
+    if download:
         create_download_directory(target_directory)
-    search_documents_from_list(browser, document_list, download_documents)
+    search_documents_from_list(browser, document_list, download)
     return abstract_dict
+
+
+def create_abstraction(browser, target_directory, file_name, sheet_name, download):
+    document_list = generate_document_list(target_directory, file_name, sheet_name)
+    abstract_dict = create_abstraction_dictionary(browser, target_directory, document_list, download)
+    export_document(target_directory, file_name, abstract_dict)
+
+
+def execute_program(browser, target_directory, file_name, sheet_name, download):
+    account_login(browser)
+    create_abstraction()
+    while continue_prompt(target_directory, file_name, sheet_name):
+        target_directory, file_name, sheet_name = \
+            request_more_information(target_directory, file_name, sheet_name)
+        create_abstraction(target_directory, file_name, sheet_name, download)
