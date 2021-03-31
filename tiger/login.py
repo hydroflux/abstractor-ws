49a38361e8d1b74c6f0b5f7e401abfc2d0d3f921
Jack Hubert <jphubert93@gmail.com>
2021-03-30 19:50:53 -0600

    updated login

--- tiger/login.py
+++ tiger/login.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-from .variables import website, website_title, credentials, timeout
+from .variables import website, website_title, credentials, timeout, handle_disclaimer_id
 
 def open_site(browser):
     browser.get(website)
@@ -19,3 +19,19 @@ def enter_credentials(browser):
         login_prompt.send_keys(credentials[0] + Keys.TAB + credentials[2] + Keys.RETURN)
     except TimeoutException:
         print("Browser timed out while trying to enter login credentials.")
+
+
+def handle_disclaimer(browser):
+    try:
+        disclaimer_present = EC.element_to_be_clickable((By.ID, handle_disclaimer_id))
+        WebDriverWait(browser, timeout).until(disclaimer_present)
+        disclaimer = browser.find_element_by_id(handle_disclaimer_id)
+        disclaimer.click()
+    except TimeoutException:
+        print("Browser timed out while trying to handle the website disclaimer.")
+
+
+def login(browser):
+    open_site(browser)
+    enter_credentials(browser)
+    handle_disclaimer(browser)
---
+++ tiger/open_document.py
@@ -0,0 +1,49 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from .variables import result_count_button_id, timeout
+
+result_count_button_id = "showCountButton"
+result_count_id = "showCountSpan"
+results_id = "resultsTable"
+results_body_tag = "tbody"
+first_result_tag = "tr"
+result_cell_tag = "tablecell"
+
+def open_result_count(browser, document_number):
+    try:
+        result_count_button_present = EC.element_to_be_clickable((By.ID, result_count_button_id))
+        WebDriverWait(browser, timeout).until(result_count_button_present)
+        result_count_button = browser.find_element_by_id(result_count_button_id)
+        result_count_button.click()
+    except TimeoutException:
+        print(f'Browser timed out trying to open the number of results for document number {document_number}.')
+
+
+def count_results(browser, document_number):
+    try:
+        result_count_present = EC.presence_of_element_located((By.ID, result_count_id))
+        WebDriverWait(browser, timeout).until(result_count_present)
+        result_count = browser.find_element_by_id(result_count_id)
+        return result_count.text.split(' ')[-1]
+    except TimeoutException:
+        print(f'Browser timed out trying to read the number of results for document number {document_number}.')
+
+
+def identify_first_result(browser, document_number):
+    try:
+        results_present = EC.presence_of_element_located((By.ID, results_id))
+        WebDriverWait(browser, timeout).until(results_present)
+        results = browser.find_element_by_id(results_id)
+        results_body = results.find_element_by_tag_name(results_body_tag)
+        return results_body.find_element_by_tag_name(first_result_tag)
+    except TimeoutException:
+        print(f'Browser timed out trying to identify first result after searching document number {document_number}.')
+
+
+def check_result(browser, document_number):
+    first_result = identify_first_result(browser, document_number)
+    first_result_cells = first_result.find_elements_by_tag_name(result_cell_tag)
+    if document_number in first_result_cells
