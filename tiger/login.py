f6d3fb7adfb9a65caa6b25d2d31173c239358192
Jack Hubert <jphubert93@gmail.com>
2021-03-30 18:08:59 -0600

    tiger login

---
+++ tiger/login.py
@@ -0,0 +1,21 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from .variables import website, website_title, credentials, timeout
+
+def open_site(browser):
+    browser.get(website)
+    assert webpage_title in browser.title
+
+
+def enter_credentials(browser):
+    try:
+        login_prompt_open = EC.presence_of_element_located((By.ID, credentials[1]))
+        WebDriverWait(browser, timeout).until(login_prompt_open)
+        login_prompt = browser.find_element_by_id(credentials[1])
+        login_prompt.send_keys(credentials[0] + Keys.TAB + credentials[2] + Keys.RETURN)
+    except TimeoutException:
+        print("Browser timed out while trying to enter login credentials.")
