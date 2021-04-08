70403a0e9f82d256f595f39468d7208cb4779839
Jack Hubert <jphubert93@gmail.com>
2021-04-08 14:59:50 -0600

    moved sleep from download to open

--- eagle/record.py
+++ eagle/record.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
