7ee67ccecfc37403a5c5880fe372e0888e0bab93
Jack Hubert <jphubert93@gmail.com>
2021-04-08 15:17:58 -0600

    updated bad_search & eagle review documents from list

--- eagle/execute.py
+++ eagle/execute.py
@@ -25,7 +25,7 @@ def search_documents_from_list(browser, county, target_directory, document_list,
         if open_document(browser, document):
             document_number = record_document(browser, abstract_dictionary, document)
             if download:
-                download_document(browser, target_directory, county, document)
+                download_document(browser, target_directory, county, document_number)
             print(f'Document located at {extrapolate_document_value(document)} recorded, '
                   f'{list_remaining_documents(document_list, document)}')
         else:
@@ -42,7 +42,6 @@ def review_documents_from_list(browser, document_list):
                   'please review & press enter to continue...'
                   f'{list_remaining_documents(document_list, document)}')
         else:
-            record_bad_search(abstract_dictionary, document)
             input(f'No document found at {extrapolate_document_value(document)}, '
                   'please review & press enter to continue...'
                   f'{list_remaining_documents(document_list, document)}')
--- settings/bad_search.py
+++ settings/bad_search.py
@@ -1,18 +1,39 @@
 if __name__ == '__main__':
+    from file_management import (document_type, document_value,
+                                 extrapolate_document_value)
     from settings.settings import search_errors
 else:
     from .settings import search_errors
 
 
-def record_bad_search(dataframe, document_number):
-    bad_search_message = f'No document found at reception number {document_number}'
+def add_bad_search_key_values(dataframe, document):
+    if document_type(document) == "document_number":
+        document_number = document_value(document)
+        dataframe["Book"].append(search_errors[2])
+        dataframe["Page"].append(search_errors[2])
+        dataframe["Reception Number"].append(document_number)
+    elif document_type(document) == "book_and_page":
+        book, page = document_value(document)
+        dataframe["Book"].append(book)
+        dataframe["Page"].append(page)
+        dataframe["Reception Number"].append(search_errors[2])
+
+
+def add_bad_search_message(dataframe, document):
+    if document_type(document) == "document_number":
+        document_number = document_value(document)
+        bad_search_message = f'No document found at reception number {document_number}, please review'
+    elif document_type(document) == "book_and_page":
+        bad_search_message = f'No document found at {extrapolate_document_value(document)}, please review'
+    dataframe["Comments"].append(bad_search_message)
+
+
+def record_bad_search(dataframe, document):
+    add_bad_search_key_values(dataframe, document)
     dataframe["Grantor"].append(search_errors[0])
     dataframe["Grantee"].append(search_errors[0])
-    dataframe["Book"].append(search_errors[2])
-    dataframe["Page"].append(search_errors[2])
-    dataframe["Reception Number"].append(document_number)
     dataframe["Document Type"].append(search_errors[0])
     dataframe["Recording Date"].append(search_errors[1])
     dataframe["Legal"].append(search_errors[2])
     dataframe["Related Documents"].append(search_errors[2])
-    dataframe["Comments"].append(bad_search_message)
+    add_bad_search_message(dataframe, document)
