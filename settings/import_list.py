88808158d04646bd50e782e652a9632d1d77b090
Jack Hubert <jphubert93@gmail.com>
2021-04-07 21:55:38 -0600

    created functions to get book number

--- settings/import_list.py
+++ settings/import_list.py
@@ -1,9 +1,10 @@
 import os
-import openpyxl
+import math
 import pandas as pd
 
 if __name__ == '__main__':
     from settings.settings import web_directory
+    from settings.classes.Document import Document
 else:
     from .settings import web_directory
 
@@ -19,8 +20,21 @@ def get_sheet_rows(excel_object):
     return rows
 
 
+def is_empty_value(value):
+    value_check = float(value)
+    return math.isnan(value_check)
+
+
+def get_book_number(row):
+    book = row['Book']
+    if is_empty_value(book):
+        return None
+    else:
+        return int(book)
+
+
 def import_excel_document(file_path, sheet_name):
-    excel_object = pd.read_excel(file_path, sheet_name, index_col=0)
+    excel_object = pd.read_excel(file_path, sheet_name)
     int_document_list = excel_object.values.flatten().tolist()
     return [str(i) for i in int_document_list]
 
