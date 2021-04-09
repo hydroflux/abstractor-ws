d1bee4fc748eea279cc88989fddec15130c154fe
Jack Hubert <jphubert93@gmail.com>
2021-04-09 16:44:41 -0600

    updated environment file to include leopard execute_program & execute_review

--- environment.py
+++ environment.py
@@ -2,12 +2,12 @@ print("environment", __name__)
 if __name__ == '__main__':
     from eagle.execute import execute_program as execute_eagle
     from eagle.execute import execute_review as review_eagle
+    from leopard.execute import execute_program as execute_leopard
+    from leopard.execute import execute_review as review_leopard
     from settings.settings import (county, download, file_name, headless,
                                    programs, sheet_name, target_directory,
                                    web_directory)
     from settings.user_prompts import get_program_type
-
-    # from tiger.execute import execute_web_program
 else:
     from .eagle.execute import execute_program as execute_eagle
     from .eagle.execute import execute_review as review_eagle
@@ -23,3 +23,10 @@ if county == programs["eagle"]:
         execute_eagle(county, target_directory, file_name, sheet_name, download)
     elif program_type == "review":
         review_eagle(target_directory, file_name, sheet_name)
+elif county == programs["leopard"]:
+    if program_type == "execute":
+        execute_leopard(headless, target_directory, county, file_name, sheet_name, download)
+    elif program_type == "review":
+        review_leopard(target_directory, file_name, sheet_name)
+else:
+    print(f'County {county} does not match available execution options, please review.')
