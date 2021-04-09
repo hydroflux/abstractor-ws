97d7ab5e8d2d5585678e7194d45030c46517585f
Jack Hubert <jphubert93@gmail.com>
2021-04-09 16:40:54 -0600

    updated leopard execute to include review functionality

--- leopard/execute.py
+++ leopard/execute.py
@@ -42,6 +42,21 @@ def search_documents_from_list(browser, county, target_directory, document_list,
                   f'{list_remaining_documents(document_list, document)}')
 
 
+def review_documents_from_list(browser, document_list):
+    for document in document_list:
+        search(browser, document)
+        if open_document(browser, document):
+            input(f'Document located at {extrapolate_document_value(document)} located,'
+                  'please review & press enter to continue...'
+                  f'{list_remaining_documents(document_list, document)}')
+            javascript_script_execution(search_script)
+            naptime()
+        else:
+            input(f'No document found at {extrapolate_document_value(document)}, '
+                  'please review & press enter to continue...'
+                  f'{list_remaining_documents(document_list, document)}')
+
+
 def create_abstraction(browser, county, target_directory, file_name, sheet_name, download):
     document_list = generate_document_list(target_directory, file_name, sheet_name)
     search_documents_from_list(browser, county, target_directory, document_list, download)
@@ -56,5 +71,15 @@ def execute_program(headless, target_directory, county, file_name, sheet_name, d
     logout(browser)
     export_document(target_directory, file_name, dictionary)
     bundle_project(target_directory, file_name)
+    browser.close()
+    quit()
+
+
+def execute_review(target_directory, file_name, sheet_name):
+    browser = create_webdriver(target_directory, False)
+    account_login(browser)
+    document_list = generate_document_list(target_directory, file_name, sheet_name)
+    review_documents_from_list(browser, document_list)
+    logout(browser)
     browser.close()
     quit()
