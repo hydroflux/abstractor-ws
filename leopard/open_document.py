c1ef615741e4f8d7ed2da1dd6db4ee8fcd38b6ed
Jack Hubert <jphubert93@gmail.com>
2021-04-09 13:46:27 -0600

    further updated open_document, needs testing but moving on

--- leopard/open_document.py
+++ leopard/open_document.py
@@ -4,12 +4,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from settings.file_management import document_value, extrapolate_document_value
 from settings.settings import timeout
-from settings.file_management import extrapolate_document_value
 
 from leopard.leopard_variables import (first_result_tag, result_cell_tag,
-                                       result_count_button_id, result_count_class,
-                                       results_body_tag, results_id)
+                                       result_count_button_id,
+                                       result_count_class, results_body_tag,
+                                       results_id)
 
 # Script is SIMILAR, but not nearly identical, to tiger open_document
 
@@ -22,3 +23,34 @@ def count_results(browser, document):
     except TimeoutException:
         print(f'Browser timed out trying to read the number of results for '
               f'{extrapolate_document_value(document)}.')
+
+
+def identify_first_result(browser, document):
+    try:
+        results_present = EC.presence_of_element_located((By.ID, results_id))
+        WebDriverWait(browser, timeout).until(results_present)
+        results = browser.find_element_by_id(results_id)
+        browser.execute_script("arguments[0].scrollIntoView();", results)
+        results_body = results.find_element_by_tag_name(results_body_tag)
+        return results_body.find_element_by_tag_name(first_result_tag)
+    except TimeoutException:
+        print(f'Browser timed out trying to identify first result after searching '
+              f'{extrapolate_document_value(document)}.')
+
+
+def get_element_text(element):
+    return element.text
+
+
+def check_result(browser, document):
+    first_result = identify_first_result(browser, document)
+    first_result_cells = first_result.find_elements_by_tag_name(result_cell_tag)
+    if document_value(document) in map(get_element_text, first_result_cells):
+        return True
+
+
+def open_document(browser, document):
+    count_results(browser, document)
+    if check_result(browser, document):
+        identify_first_result(browser, document).click()
+        return True
