b51d7368b353df3c60e9bef0692facc2b6fc9e38
Jack Hubert <jphubert93@gmail.com>
2021-04-09 11:03:02 -0600

    updated import_list to check for different document columns

--- settings/import_list.py
+++ settings/import_list.py
@@ -6,7 +6,7 @@ import pandas as pd
 print("import_list", __name__)
 
 from settings.settings import web_directory
-from settings.classes.Document import Document
+from settings.classes.Documents import Documents as Document
 
 
 def get_sheet_columns(excel_object):
@@ -41,8 +41,15 @@ def get_page_number(row):
         return int(page)
 
 
-def get_document_number(row):
-    document_number = row['Document']
+def get_document_number(columns, row):
+    if 'Document' in columns:
+        document_number = row['Document']
+    elif 'Documents' in columns:
+        document_number = row['Documents']
+    elif 'Reception Number' in columns:
+        document_number = row['Reception Number']
+    elif 'Reception Numbers' in columns:
+        document_number = row['Reception Numbers']
     if is_empty_value(document_number):
         return None
     else:
@@ -51,6 +58,8 @@ def get_document_number(row):
 
 def store_document(document_list, type, value):
     document = Document(type=type, value=value)
+    print(document.type)
+    print(document.value)
     document_list.append(document)
 
 
@@ -65,8 +74,8 @@ def create_book_and_page_object(document_list, row):
         store_document(document_list, "book_and_page", book_and_page)
 
 
-def create_document_number_object(document_list, row):
-    document_number = get_document_number(row)
+def create_document_number_object(document_list, columns, row):
+    document_number = get_document_number(columns, row)
     if document_number is not None:
         store_document(document_list, "document_number", document_number)
 
@@ -78,10 +87,12 @@ def create_document_list(excel_object):
     for row in rows:
         if 'Book' in columns and 'Page' in columns:
             create_book_and_page_object(document_list, row)
+            input("Continue? (book_and_page)")
         if 'Document' in columns or 'Documents' in columns or \
                                     'Reception Number' in columns or \
                                     'Reception Numbers' in columns:
-            create_document_number_object(document_list, row)
+            create_document_number_object(document_list, columns, row)
+            input("Continue? (document_number)")
     return document_list
 
 
