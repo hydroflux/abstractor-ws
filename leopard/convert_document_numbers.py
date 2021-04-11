2949f65383b758dc480345726ee782083c80dd99
Jack Hubert <jphubert93@gmail.com>
2021-04-11 15:32:59 -0600

    updated leopard convert

--- leopard/convert_document_numbers.py
+++ leopard/convert_document_numbers.py
@@ -1,23 +1,23 @@
 def convert_document_numbers(document_list):
     for document in document_list:
         if document.type == "document_number":
-            doc_number, year = document.split('-')
+            document_number, year = document.value.split('-')
             year = int(year)
             if year <= 1984:
-                new_name = doc_number
-            elif int(doc_number) <= 9:
-                new_name = ('{}000000{}'.format(year, doc_number))
-            elif int(doc_number) <= 99:
-                new_name = ('{}00000{}'.format(year, doc_number))
-            elif int(doc_number) <= 999:
-                new_name = ('{}0000{}'.format(year, doc_number))
-            elif int(doc_number) <= 9999:
-                new_name = ('{}000{}'.format(year, doc_number))
-            elif int(doc_number) <= 99999:
-                new_name = ('{}00{}'.format(year, doc_number))
-            elif int(doc_number) <= 999999:
-                new_name = ('{}0{}'.format(year, doc_number))
+                new_name = document_number
+            elif int(document_number) <= 9:
+                new_name = ('{}000000{}'.format(year, document_number))
+            elif int(document_number) <= 99:
+                new_name = ('{}00000{}'.format(year, document_number))
+            elif int(document_number) <= 999:
+                new_name = ('{}0000{}'.format(year, document_number))
+            elif int(document_number) <= 9999:
+                new_name = ('{}000{}'.format(year, document_number))
+            elif int(document_number) <= 99999:
+                new_name = ('{}00{}'.format(year, document_number))
+            elif int(document_number) <= 999999:
+                new_name = ('{}0{}'.format(year, document_number))
             else:
-                new_name = ('{}{}'.format(year, doc_number))
+                new_name = ('{}{}'.format(year, document_number))
                 print(new_name)
             document.value = new_name
