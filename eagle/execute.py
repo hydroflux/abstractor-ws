ee3b03c4adf8362bbc30ce89d3134d249bd1c123
Jack Hubert <jphubert93@gmail.com>
2021-05-04 16:42:17 -0600

    created review single & review multiple document functions to mirror record single & record multiple document functions in eagle record

--- eagle/execute.py
+++ eagle/execute.py
@@ -27,9 +27,9 @@ def search_documents_from_list(browser, county, target_directory, document_list,
         document_search(browser, document)
         if open_document(browser, document):
             if document.number_results > 1:
-                record_multiple_documents(browser, county, target_directory, abstract_dictionary, document)
+                record_multiple_documents(browser, county, target_directory, abstract_dictionary, document_list, document)
             else:
-                record_single_document(browser, county, target_directory, abstract_dictionary, document)
+                record_single_document(browser, county, target_directory, abstract_dictionary, document_list, document)
         else:
             record_bad_search(abstract_dictionary, document)
             print(f'No document found at {extrapolate_document_value(document)}, '
@@ -37,7 +37,7 @@ def search_documents_from_list(browser, county, target_directory, document_list,
     return abstract_dictionary
 
 
-def record_single_document(browser, county, target_directory, abstract_dictionary, document):
+def record_single_document(browser, county, target_directory, abstract_dictionary, document_list, document):
     document_number = record_document(browser, abstract_dictionary, document)
     if download:
         download_document(browser, county, target_directory, document_number)
@@ -45,7 +45,7 @@ def record_single_document(browser, county, target_directory, abstract_dictionar
     f'{list_remaining_documents(document_list, document)}')
 
 
-def record_multiple_documents(browser, county, target_directory, abstract_dictionary, document):
+def record_multiple_documents(browser, county, target_directory, abstract_dictionary, document_list, document):
     for document in range(0, document.number_results):
         record_single_document(browser, county, target_directory, abstract_dictionary, document)
         next_result(browser, document)
@@ -55,14 +55,26 @@ def review_documents_from_list(browser, document_list):
     for document in document_list:
         document_search(browser, document)
         if open_document(browser, document):
-            for document in range(0, document.number_results):
-                input(f'Document located at {extrapolate_document_value(document)} located,'
-                    'please review & press enter to continue...'
-                    f'{list_remaining_documents(document_list, document)}')
+            if document.number_results > 1:
+                review_single_document(document_list, document)
+            else:
+                review_multiple_documents(document_list, document)
         else:
             input(f'No document found at {extrapolate_document_value(document)}, '
                   'please review & press enter to continue...'
                   f'{list_remaining_documents(document_list, document)}')
+
+
+def review_single_document(document_list, document):
+    input(f'Document located at {extrapolate_document_value(document)} located,'
+                'please review & press enter to continue...'
+                f'{list_remaining_documents(document_list, document)}')
+
+
+def review_multiple_documents(document_list, document):
+    for document in range(0, document.number_results):
+        review_single_document(document_list, document)
+        next_result(browser, document)
 
 
 def create_abstraction(browser, county, target_directory, file_name, sheet_name, download):
