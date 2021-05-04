d4031302d9273e1b971052113567c2167247f3b3
Jack Hubert <jphubert93@gmail.com>
2021-05-04 15:00:53 -0600

    removed all verification functions from eagle open_document, testing is required, but ideally will move verification to occur prior to recording

--- eagle/open_document.py
+++ eagle/open_document.py
@@ -78,7 +78,7 @@ def count_results(browser):
     if search_results == no_results:
         return 0
     else:
-        return int(len(get_results(browser)))
+        return int(len(get_search_results(browser)))
 
 
 def check_search_results(browser, document):
@@ -101,75 +101,75 @@ def check_search_results(browser, document):
 #         print("Browser timed out while trying to retrieve the first result of the search.")
 
 
-def get_first_result_info(browser):
-    first_result = get_first_result(browser)
-    if first_result is None:
-        return None
-    first_result_info = first_result.find_element_by_tag_name(first_result_tag)
-    browser.execute_script("arguments[0].scrollIntoView();", first_result_info)
-    return first_result_info
+# def get_first_result_info(browser):
+#     first_result = get_first_result(browser)
+#     if first_result is None:
+#         return None
+#     first_result_info = first_result.find_element_by_tag_name(first_result_tag)
+#     browser.execute_script("arguments[0].scrollIntoView();", first_result_info)
+#     return first_result_info
 
 
-def get_first_result_nested_info(browser, first_result_info):
-    try:
-        first_result_nested_info_present = EC.presence_of_element_located((By.CLASS_NAME, first_result_submenu_class))
-        WebDriverWait(browser, timeout).until(first_result_nested_info_present)
-        first_result_nested_info = browser.find_elements_by_class_name(first_result_submenu_class)
-        return first_result_nested_info
-    except TimeoutException:
-        print(f'Browser timed out while trying to get nested information from "{first_result_info.text}".')
+# def get_first_result_nested_info(browser, first_result_info):
+#     try:
+#         first_result_nested_info_present = EC.presence_of_element_located((By.CLASS_NAME, first_result_submenu_class))
+#         WebDriverWait(browser, timeout).until(first_result_nested_info_present)
+#         first_result_nested_info = browser.find_elements_by_class_name(first_result_submenu_class)
+#         return first_result_nested_info
+#     except TimeoutException:
+#         print(f'Browser timed out while trying to get nested information from "{first_result_info.text}".')
 
 
-def split_book_and_page_info(book_and_page_info):
-    book_and_page_values = book_and_page_info[-1].text.split()
-    if book_and_page_values[0] == book_title and book_and_page_values[2] == page_title:
-        return [book_and_page_values[1], book_and_page_values[3]]
-    else:
-        print(f'Book & page titles do not line up as expected, instead got "{book_and_page_values}", please review.')
-        return False
+# def split_book_and_page_info(book_and_page_info):
+#     book_and_page_values = book_and_page_info[-1].text.split()
+#     if book_and_page_values[0] == book_title and book_and_page_values[2] == page_title:
+#         return [book_and_page_values[1], book_and_page_values[3]]
+#     else:
+#         print(f'Book & page titles do not line up as expected, instead got "{book_and_page_values}", please review.')
+#         return False
 
 
 # Hitting a snag on clicking the first result info, please review
-def expand_first_result_nested_info(browser, first_result_info):
-    try:
-        browser.find_element_by_class_name(nested_submenu_class)
-    except NoSuchElementException:
-        try:
-            scroll_into_view(browser, first_result_info)
-            first_result_info.click()
-        except ElementClickInterceptedException:
-            print("Element click intercepted, trying again.")
-            get_first_result_info(browser).click()
+# def expand_first_result_nested_info(browser, first_result_info):
+#     try:
+#         browser.find_element_by_class_name(nested_submenu_class)
+#     except NoSuchElementException:
+#         try:
+#             scroll_into_view(browser, first_result_info)
+#             first_result_info.click()
+#         except ElementClickInterceptedException:
+#             print("Element click intercepted, trying again.")
+#             get_first_result_info(browser).click()
 
 
-def get_book_and_page_values(browser, first_result_info):
-    expand_first_result_nested_info(browser, first_result_info)
-    nested_info = get_first_result_nested_info(browser, first_result_info)
-    book_and_page_info = nested_info[-1].find_elements_by_tag_name(book_and_page_tag)
-    return split_book_and_page_info(book_and_page_info)
+# def get_book_and_page_values(browser, first_result_info):
+#     expand_first_result_nested_info(browser, first_result_info)
+#     nested_info = get_first_result_nested_info(browser, first_result_info)
+#     book_and_page_info = nested_info[-1].find_elements_by_tag_name(book_and_page_tag)
+#     return split_book_and_page_info(book_and_page_info)
 
 
-def get_first_result_value(browser, document):
-    first_result_info = get_first_result_info(browser)
-    if first_result_info is None:
-        return False
-    if document_type(document) == "document_number":
-        return first_result_info.text.split(" ")[0]
-    elif document_type(document) == "book_and_page":
-        try:
-            return get_book_and_page_values(browser, first_result_info)
-        except IndexError:
-            print(f'Encountered index error while splitting book & page for '
-                  f'{extrapolate_document_value(document)}, please review.')
+# def get_first_result_value(browser, document):
+#     first_result_info = get_first_result_info(browser)
+#     if first_result_info is None:
+#         return False
+#     if document_type(document) == "document_number":
+#         return first_result_info.text.split(" ")[0]
+#     elif document_type(document) == "book_and_page":
+#         try:
+#             return get_book_and_page_values(browser, first_result_info)
+#         except IndexError:
+#             print(f'Encountered index error while splitting book & page for '
+#                   f'{extrapolate_document_value(document)}, please review.')
 
 
-def verify_first_result_number(document, first_result_value):
-    if first_result_value == document_value(document):
-        return True
-    else:
-        print(f'First result Document number {first_result_value} does not match '
-              f'searched {extrapolate_document_value(document)}, please review.')
-        return False
+# def verify_first_result_number(document, first_result_value):
+#     if first_result_value == document_value(document):
+#         return True
+#     else:
+#         print(f'First result Document number {first_result_value} does not match '
+#               f'searched {extrapolate_document_value(document)}, please review.')
+#         return False
 
 
 # def verify_first_result_book_and_page(document, first_result_value):
@@ -187,15 +187,15 @@ def verify_first_result_number(document, first_result_value):
 
 
 # Verify book & page is broken, need to assess verification on documents prior to 1900
-def verify_result(browser, document):
-    first_result_value = get_first_result_value(browser, document)
-    if first_result_value is False:
-        return False
-    if document_type(document) == "document_number":
-        return verify_first_result_number(document, first_result_value)
-    elif document_type(document) == "book_and_page":
-        # return verify_first_result_book_and_page(document, first_result_value)
-        return True
+# def verify_result(browser, document):
+#     first_result_value = get_first_result_value(browser, document)
+#     if first_result_value is False:
+#         return False
+#     if document_type(document) == "document_number":
+#         return verify_first_result_number(document, first_result_value)
+#     elif document_type(document) == "book_and_page":
+#         # return verify_first_result_book_and_page(document, first_result_value)
+#         return True
 
 
 def view_search_actions(browser, first_result):
@@ -229,7 +229,7 @@ def open_document_description(browser, first_result):
 def open_document(browser, document):
     if check_search_results(browser, document):    
         try:
-            first_result = get_results(browser)[0]
+            first_result = get_search_results(browser)[0]
             open_document_description(browser, get_first_result(browser))
             naptime()
             return True
