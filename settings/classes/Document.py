b53d3e916476eb70bd50cb49e8917774673fa8bb
Jack Hubert <jphubert93@gmail.com>
2021-05-05 10:11:08 -0600

    fixed a typo in the Document class

--- settings/classes/Document.py
+++ settings/classes/Document.py
@@ -1,5 +1,5 @@
 class Document:
-    def __init__(self, type, value, multiple=None):
+    def __init__(self, type, value, number_results=None):
         self.type = type
         self.value = value
         self.number_results = number_results
