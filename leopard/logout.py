4e3ee0d02605ae07f89452d15ddf9ed89202d579
Jack Hubert <jphubert93@gmail.com>
2021-05-12 14:53:01 -0600

    refactored leopard logout

--- leopard/logout.py
+++ leopard/logout.py
@@ -8,12 +8,22 @@ from settings.general_functions import timeout
 # Use the following print statement to identify the best way to manage imports for Django vs the script folder
 print("logout", __name__)
 
-def logout(browser):
+
+def locate_logout_button(browser):
     try:
         logout_button_present = EC.presence_of_element_located((By.ID, logout_button_id))
         WebDriverWait(browser, timeout).until(logout_button_present)
         logout_button = browser.find_element_by_id(logout_button_id)
-        logout_link = logout_button.get_attribute("href")
-        browser.get(logout_link)
+        return logout_button
     except TimeoutException:
-        print("Browser timed out while trying to logout, please review.")
+        print("Browser timed out while trying to locate logout button, please review.")
+
+
+def get_logout_link(browser):
+    logout_button = locate_logout_button(browser)
+    return logout_button.get_attribute("href")
+
+
+def logout(browser):
+    logout_link = get_logout_link(browser)
+    browser.get(logout_link)
