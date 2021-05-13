84c480da386e5c9fec83712cca0ec2c9484894a6
Jack Hubert <jphubert93@gmail.com>
2021-05-13 13:18:54 -0600

    added transform_list

---
+++ leopard/transform_document_list.py
@@ -0,0 +1,28 @@
+def convert_document_numbers(document_list):
+    for document in document_list:
+        if document.type == "document_number" and document.value.find("-") != -1:
+            document_number, year = document.value.split('-')
+            year = int(year)
+            if year <= 1984:
+                new_name = document_number
+            elif int(document_number) <= 9:
+                new_name = ('{}000000{}'.format(year, document_number))
+            elif int(document_number) <= 99:
+                new_name = ('{}00000{}'.format(year, document_number))
+            elif int(document_number) <= 999:
+                new_name = ('{}0000{}'.format(year, document_number))
+            elif int(document_number) <= 9999:
+                new_name = ('{}000{}'.format(year, document_number))
+            elif int(document_number) <= 99999:
+                new_name = ('{}00{}'.format(year, document_number))
+            elif int(document_number) <= 999999:
+                new_name = ('{}0{}'.format(year, document_number))
+            else:
+                new_name = ('{}{}'.format(year, document_number))
+            document.value = new_name
+            document.year = year
+
+
+# Redundant function as it stands, the purpose being to circle back & add additional functionality
+def transform_document_list(document_list):
+    convert_document_numbers(document_list)
