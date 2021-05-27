9c11111ded99c18f511adb31265b17a768b35212
Jack Hubert <jphubert93@gmail.com>
2021-05-27 14:56:12 -0600

    removed commented out search_wait from general_functions & added a comment for naptime--probably won't update again but needs to be considered as naptime is used across all scripts

--- settings/general_functions.py
+++ settings/general_functions.py
@@ -7,15 +7,13 @@ from settings.classes.counties import county_dictionary
 from settings.settings import abstraction_type
 
 # Timeout / Wait Variables
-# search_wait = sleep(randint(2, 5)) # Commented out, delete if runtime continues uninhibited
 timeout = randint(20, 30)
 long_timeout = randint(120, 180)
 short_timeout = randint(5, 10)
 
 
 def naptime():
-    # sleep(randint(3, 6))
-    sleep(randint(3, 4))
+    sleep(randint(3, 4))  # Consider adding an additional second if running into issues while using naps
 
 
 def short_nap():
