bc5456c9b7776488d25ca74550dcd9bcfbafe789
Jack Hubert <jphubert93@gmail.com>
2021-06-08 11:06:06 -0600

    created an open_document script for crocodile & created a function skeleton to fill out

---
+++ crocodile/open_document.py
@@ -0,0 +1,15 @@
+def count_total_results(browser, document):
+    pass
+
+
+def open_document_link(browser):
+    pass
+
+
+def verify_results(browser, document):
+    pass
+
+
+def open_document(browser, document):
+    count_total_results(browser, document)
+    return verify_results(browser, document)
