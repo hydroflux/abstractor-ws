0f0ec3e82710e47ccdfa0affc3be235d5d1a3eee
Jack Hubert <jphubert93@gmail.com>
2021-06-08 10:07:41 -0600

    created a function to locate the login prompt for crocodile login

--- crocodile/login.py
+++ crocodile/login.py
@@ -5,9 +5,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from settings.general_functions import timeout
 
-from crocodile.crocodile_variables import website, website_title
+from crocodile.crocodile_variables import website, website_title, credentials
 
 
 def open_site(browser):
     browser.get(website)
-    assert website_title in browser.title
+    assert website_title in browser.title.strip()
+
+
+def locate_login_prompt(browser):
+    try:
+        login_prompt_present = EC.presence_of_element_located((By.ID, credentials[0]))
+        WebDriverWait(browser, timeout).until(login_prompt_present)
+        login_prompt = browser.find_element_by_id(credentials[0])
+        return login_prompt
+    except TimeoutException:
+        print("Browser timed out trying to locate login prompt, please review.")
