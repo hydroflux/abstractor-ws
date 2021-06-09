c5e3eabfec2a5a0a21df6f068676bd2845566956
Jack Hubert <jphubert93@gmail.com>
2021-06-09 13:22:11 -0600

    created a download script for crocodile & built a skeleton function structure to build out from

---
+++ crocodile/download.py
@@ -0,0 +1,30 @@
+import os
+
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from settings.download_management import previously_downloaded, update_download
+from settings.file_management import (create_document_directory,
+                                      extrapolate_document_value)
+from settings.general_functions import timeout
+
+
+def open_document_page():
+    pass
+
+
+def locate_download_menu():
+    pass
+
+
+def locate_download_button():
+    pass
+
+
+def execute_download():
+    pass
+
+
+def download_document():
+    pass
