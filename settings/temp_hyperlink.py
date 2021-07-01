a4100799c219d17e682e099e8a626a5b34d81cff
Jack Hubert <jphubert93@gmail.com>
2021-07-01 17:34:07 -0600

    created temp_hyperlink skeleton

--- settings/temp_hyperlink.py
+++ settings/temp_hyperlink.py
@@ -1,6 +1,19 @@
+import os
+
 # The purposed of this script is to create hyperlinks for abstract exports
 # it's "temporary" because the ideal situation will be to add hyperlinks directly into
 # final product, but hit a roadblock which needs to be addressed later
 
-def write_temporary_hyperlinks(county, document_directory, hyperlink_sheet, hyperlink_format):
+
+def drop_ds_store():
     pass
+
+
+def write_hyperlink_url():
+    pass
+
+
+def write_temporary_hyperlinks(county, document_directory, hyperlink_sheet, hyperlink_format):
+    for file in sorted(os.listdir(document_directory)):
+        if not drop_ds_store():
+            write_hyperlink_url()
