190d963b7eee8b558e1215ba1e6636d522e6e11b
Jack Hubert <jphubert93@gmail.com>
2021-07-14 12:43:53 -0600

    hit mvp for buffalo login

--- buffalo/login.py
+++ buffalo/login.py
@@ -1,10 +1,11 @@
+from buffalo.frame_handling import switch_to_main_frame
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from settings.general_functions import assert_window_title, fill_search_field, javascript_script_execution, timeout
 
-from buffalo.buffalo_variables import (credentials, website, website_title, login_script, disclaimer_button_id)
+from buffalo.buffalo_variables import (credentials, website, website_title, login_script, disclaimer_button_id, disclaimer_script, post_login_title, post_login_text, welcome_message_id)
 
 
 def open_site(browser):
@@ -35,6 +36,7 @@ def submit_password(browser):
 
 def execute_login(browser):
     javascript_script_execution(browser, login_script)
+    assert_window_title(browser, post_login_title)
 
 
 def locate_disclaimer_button(browser):
@@ -48,12 +50,25 @@ def locate_disclaimer_button(browser):
 
 
 def handle_disclaimer(browser):
-    disclaimer_button = locate_disclaimer_button(browser)
-    disclaimer_button.click()
+    switch_to_main_frame(browser)
+    javascript_script_execution(browser, disclaimer_script)
+
+
+def locate_post_login_message(browser):
+    try:
+        welcome_message_present = EC.presence_of_element_located((By.ID, welcome_message_id))
+        WebDriverWait(browser, timeout).until(welcome_message_present)
+        welcome_message = browser.find_element_by_id(welcome_message_id)
+        return welcome_message
+    except TimeoutException:
+        print("Browser timed out trying to locate post login welcome message, please review.")
 
 
 def verify_login(browser):
-    pass
+    switch_to_main_frame(browser)
+    welcome_message = locate_post_login_message(browser)
+    if welcome_message.text == post_login_text:
+        print('\nLogin successful, continuing program execution.')
 
 
 def account_login(browser):
@@ -61,4 +76,5 @@ def account_login(browser):
     submit_username(browser)
     submit_password(browser)
     execute_login(browser)
+    handle_disclaimer(browser)
     verify_login(browser)
