a7fc4680bb6b033f4eebf118bdc3de3c4ad0d135
Jack Hubert <jphubert93@gmail.com>
2021-07-14 12:12:22 -0600

    continued working on buffalo login, adding a submit_username & submit_password function (using fill_search_field) & created the skeleton structure for account_login (matches most closely to crocodile)

--- buffalo/login.py
+++ buffalo/login.py
@@ -2,7 +2,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from settings.general_functions import assert_window_title, timeout
+from settings.general_functions import assert_window_title, fill_search_field, timeout
 
 from buffalo.buffalo_variables import (credentials, website, website_title)
 
@@ -23,3 +23,27 @@ def locate_login_field(browser, prompt_name, type):
         return login_prompt
     except TimeoutException:
         print(f'Browser timed out trying to locate {type} prompt, please review.')
+
+
+def submit_username(browser):
+    fill_search_field(locate_login_field(browser, credentials[0], "username"), credentials[1])
+
+
+def submit_password(browser):
+    fill_search_field(locate_login_field(browser, credentials[2], "password"), credentials[3])
+
+
+def execute_login(browser):
+    pass
+
+
+def verify_login(browser):
+    pass
+
+
+def account_login(browser):
+    open_site(browser)
+    submit_username(browser)
+    submit_password(browser)
+    execute_login(browser)
+    verify_login(browser)
