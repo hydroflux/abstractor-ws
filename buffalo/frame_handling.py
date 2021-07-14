502da66297753611b49aa106b5f8c5596d68024e
Jack Hubert <jphubert93@gmail.com>
2021-07-14 12:34:31 -0600

    updated frame_handling to enable switching into the main frame of the DOM body

--- buffalo/frame_handling.py
+++ buffalo/frame_handling.py
@@ -2,9 +2,22 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from settings.general_functions import timeout
 
-def locate_frame_by_name(browser):
-    pass
+from buffalo.buffalo_variables import main_frame_name
+
+
+def locate_iframe_by_name(browser, iframe_name):
+    try:
+        iframe_present = EC.presence_of_element_located((By.NAME, iframe_name))
+        WebDriverWait(browser, timeout).until(iframe_present)
+        iframe = browser.find_element_by_name(iframe_name)
+        return iframe
+    except TimeoutException:
+        print(f'Browser timed out trying to locate {iframe_name}, please review.')
+
 
 def switch_to_main_frame(browser):
-    pass
+    browser.switch_to.default_content()
+    main_frame = locate_iframe_by_name(browser, main_frame_name)
+    browser.switch_to.frame(main_frame)
