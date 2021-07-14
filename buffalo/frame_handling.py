1e667bbf4d238f22de00713fb329e9749ebd3c3c
Jack Hubert <jphubert93@gmail.com>
2021-07-14 12:28:56 -0600

    created a 'frame_handling' script for buffalo to help bouncing back and forth between the nested frames of the buffalo site

---
+++ buffalo/frame_handling.py
@@ -0,0 +1,10 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+def locate_frame_by_name(browser):
+    pass
+
+def switch_to_main_frame(browser):
+    pass
