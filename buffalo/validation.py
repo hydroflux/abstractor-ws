6c3e5b6c727e63255b51ae8d7f63223fc1528414
Jack Hubert <jphubert93@gmail.com>
2021-07-22 10:31:44 -0600

    added a validation script to use in order to validate the search criteria vs the search results; created new script with intention of adding additional validation functions later on

---
+++ buffalo/validation.py
@@ -0,0 +1,24 @@
+from buffalo.frame_handling import switch_to_header_frame
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from settings.general_functions import timeout
+
+from buffalo.buffalo_variables import header_text_tag
+
+
+def locate_header_text(browser):
+    try:
+        header_text_element_present = EC.presence_of_element_located((By.TAG_NAME, header_text_tag))
+        WebDriverWait(browser, timeout).until(header_text_element_present)
+        header_text_element = browser.find_element_by_tag_name(header_text_tag)
+        return header_text_element
+    except TimeoutException:
+        print('Browser timed out trying to located header text during header validation, please review.')
+
+
+def header_validation(browser, validation_text):
+    switch_to_header_frame(browser)
+    header_text_element = locate_header_text(browser)
+    return header_text_element.startswith(validation_text)
