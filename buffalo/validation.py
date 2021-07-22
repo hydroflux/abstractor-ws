f06827b3f8a73b4059cee6d5a44929551f4559ff
Jack Hubert <jphubert93@gmail.com>
2021-07-22 10:57:47 -0600

    added an additional validation function in order to handle if the page is loaded correctly (indicated by the header)

--- buffalo/validation.py
+++ buffalo/validation.py
@@ -22,3 +22,12 @@ def header_validation(browser, validation_text):
     switch_to_header_frame(browser)
     header_text_element = locate_header_text(browser).text
     return header_text_element.startswith(validation_text)
+
+
+def page_is_loaded(browser, validation_text):
+    if not header_validation(browser, validation_text):
+        print(f'Browser indicates that it has not properly loaded '
+              f'"{validation_text}", please review webdriver before continuing...')
+        input()
+    else:
+        return True
