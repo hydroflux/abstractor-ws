5abe44a52b0ea2b7e8f8f7c54f8621a94005756d
Jack Hubert <jphubert93@gmail.com>
2021-07-27 14:18:01 -0600

    created a function skeleton for buffalo open_document

--- buffalo/open_document.py
+++ buffalo/open_document.py
@@ -2,6 +2,18 @@ from buffalo.validation import page_is_loaded
 from buffalo.buffalo_variables import search_results_header_text
 
 
-def open_document(browser):
+def verify_document_number(browser, document):
+    pass
+
+
+def open_document_number(browser, document):
+    pass
+
+
+def process_open_document(browser, document):
+    pass
+
+
+def open_document(browser, document):
     if page_is_loaded(browser, search_results_header_text):
-        pass
+        process_open_document(browser, document)
