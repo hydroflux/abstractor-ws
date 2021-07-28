c84705f0afebed87c4c970aa2eea46eef680768b
Jack Hubert <jphubert93@gmail.com>
2021-07-28 09:17:11 -0600

    set up record_single_document in buffalo execute, commenting out functions that have not yet been created--next steps => record_document script & download_document script

--- buffalo/execute.py
+++ buffalo/execute.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from settings.abstract_object import abstract_dictionary as dictionary
-from settings.bad_search import record_bad_search
+from settings.bad_search import record_bad_search, unable_to_download
 from settings.driver import create_webdriver
 from settings.export import export_document
 from settings.file_management import (bundle_project, check_length, document_found, extrapolate_document_value,
@@ -15,7 +15,11 @@ from buffalo.search import search
 
 
 def record_single_document(browser, county, target_directory, document_list, document, start_time):
-    pass
+    # record_document(browser, county, dictionary, document)
+    # if download:
+        # if not download_document(browser, county, target_directory, document):
+            # unable_to_download(dictionary, document)
+    document_found(start_time, document_list, document)
 
 
 def handle_search_results(browser, county, target_directory, document_list, document, start_time):
