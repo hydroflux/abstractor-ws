2706f0788eb76d1edcbe572f1034a35bef57d1fe
Jack Hubert <jphubert93@gmail.com>
2021-07-28 09:19:04 -0600

    updated buffalo execute impors

--- buffalo/execute.py
+++ buffalo/execute.py
@@ -3,7 +3,9 @@ from settings.abstract_object import abstract_dictionary as dictionary
 from settings.bad_search import record_bad_search, unable_to_download
 from settings.driver import create_webdriver
 from settings.export import export_document
-from settings.file_management import (bundle_project, check_length, document_found, extrapolate_document_value,
+from settings.file_management import (bundle_project, check_length,
+                                      document_found,
+                                      extrapolate_document_value,
                                       no_document_found)
 from settings.general_functions import start_timer
 from settings.settings import download
