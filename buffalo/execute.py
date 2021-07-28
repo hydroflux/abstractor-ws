cd8cf4b29de9e89ef8e463d0f202efb92e9037f4
Jack Hubert <jphubert93@gmail.com>
2021-07-28 08:57:19 -0600

    updated buffalo excute 'execute_program' function

--- buffalo/execute.py
+++ buffalo/execute.py
@@ -26,5 +26,12 @@ def search_documents_from_list(browser, county, target_directory, document_list)
     pass
 
 
+# Identical to crocodile execute_program
 def execute_program(county, target_directory, document_list, file_name):
-    pass
+    browser = create_webdriver(target_directory, False)
+    account_login(browser)
+    dictionary = search_documents_from_list(browser, county, target_directory, document_list)
+    logout(browser)
+    abstraction = export_document(county, target_directory, file_name, dictionary)
+    bundle_project(target_directory, abstraction)
+    browser.close()
