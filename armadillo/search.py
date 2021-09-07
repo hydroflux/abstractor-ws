3eb4e9053428bdc3b19fe22a487c9b02ebdff757
Jack Hubert <jphubert93@gmail.com>
2021-09-07 13:36:48 -0600

    added a search script to use for armadillo & created a skeleton function structure to fill out using crododile search as a base to work from

---
+++ armadillo/search.py
@@ -0,0 +1,43 @@
+from settings.file_management import document_type
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def open_document_search(browser, document):
+    pass
+
+
+def locate_document_search_field(browser, document):
+    pass
+
+
+def clear_document_search_field(browser, document):
+    pass
+
+
+def enter_document_number(browser, document):
+    pass
+
+
+def handle_document_search_field(browser, document):
+    pass
+
+
+def locate_search_button(browser, document):
+    pass
+
+
+def execute_search(browser, document):
+    pass
+
+
+def document_search(browser, document):
+    pass
+
+
+# Matches crocodile 'search'
+def search(browser, document):
+    pass
