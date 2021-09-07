05b959b5d12fd4992c9a2013c173eca2eb61afa6
Jack Hubert <jphubert93@gmail.com>
2021-09-07 14:53:12 -0600

    updated count_results function in armadillo open_document--currently set up to only handle search results which return a single result

--- armadillo/open_document.py
+++ armadillo/open_document.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from settings.file_management import document_value, extrapolate_document_value
 from settings.general_functions import assert_window_title, timeout
 
-from armadillo.armadillo_variables import number_results_class, search_results_id, document_search_results_title
+from armadillo.armadillo_variables import number_results_class, search_results_id, document_search_results_title, single_result_message
 
 
 def verify_successful_search(browser, document):
@@ -15,13 +15,29 @@ def verify_successful_search(browser, document):
         input()
 
 
+def locate_result_count(browser, document):
+    try:
+        result_count_present = EC.presence_of_element_located((By.CLASS_NAME, number_results_class))
+        WebDriverWait(browser, timeout).until(result_count_present)
+        result_count = browser.find_element_by_class_name(number_results_class)
+        return result_count
+    except TimeoutException:
+        print(f'Browser timed out trying to locate result count for '
+              f'{extrapolate_document_value(document)}, please review.')
+
+
 def count_results(browser, document):
-    pass # assert_window_title
-
-
-def verify_result_count(browser, document):
-    pass
+    result_count = locate_result_count(browser, document)
+    if result_count == single_result_message:
+        return 1
+    else:
+        print(f'Browser located multiple search results for '
+              f'{extrapolate_document_value(document)}, new logic path needs to be developed.')
+        print("Please press enter after reviewing the search parameters...")
+        input()
 
 
 def open_document(browser, document):
     verify_successful_search(browser, document)
+    if count_results == 1:
+        pass
