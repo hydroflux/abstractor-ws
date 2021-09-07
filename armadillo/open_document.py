55c00143f28cbe977dd71cf3840aa8bf360ccf68
Jack Hubert <jphubert93@gmail.com>
2021-09-07 14:46:43 -0600

    created a new function in armadillo open_document to verify that the document search was executed correctly

--- armadillo/open_document.py
+++ armadillo/open_document.py
@@ -2,10 +2,17 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from settings.file_management import extrapolate_document_value
-from settings.general_functions import timeout
+from settings.file_management import document_value, extrapolate_document_value
+from settings.general_functions import assert_window_title, timeout
 
-from armadillo.armadillo_variables import number_results_class, search_results_id
+from armadillo.armadillo_variables import number_results_class, search_results_id, document_search_results_title
+
+
+def verify_successful_search(browser, document):
+    if not assert_window_title(browser, document_search_results_title):
+        print(f'Browser failed to successfully execute search for '
+              f'{extrapolate_document_value(document)}, please review.')
+        input()
 
 
 def count_results(browser, document):
@@ -17,4 +24,4 @@ def verify_result_count(browser, document):
 
 
 def open_document(browser, document):
-    pass
+    verify_successful_search(browser, document)
