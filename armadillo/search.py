b91db24fa0af65d0b75965be3fdda9369fb7fbae
Jack Hubert <jphubert93@gmail.com>
2021-09-07 14:31:29 -0600

    updated armadillo 'search' imports

--- armadillo/search.py
+++ armadillo/search.py
@@ -3,10 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from settings.general_functions import assert_window_title, get_field_value, javascript_script_execution, timeout
-from settings.file_management import document_type, document_value, extrapolate_document_value
+from settings.file_management import (document_type, document_value,
+                                      extrapolate_document_value)
+from settings.general_functions import (assert_window_title, get_field_value,
+                                        javascript_script_execution, timeout)
 
-from armadillo.armadillo_variables import document_search_url, document_search_title, document_search_field_id, execute_document_search_script
+from armadillo.armadillo_variables import (document_search_field_id,
+                                           document_search_title,
+                                           document_search_url,
+                                           execute_document_search_script)
 
 
 def open_document_search(browser, document):
