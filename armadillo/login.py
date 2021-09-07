659b7bc8490fa2d03db8d14fa37b3d602629f7e6
Jack Hubert <jphubert93@gmail.com>
2021-09-07 12:45:50 -0600

    filled out functions for open_site & locate_login_field

--- armadillo/login.py
+++ armadillo/login.py
@@ -5,16 +5,28 @@ from selenium.webdriver.support.wait import WebDriverWait
 from settings.general_functions import (assert_window_title, fill_search_field,
                                         timeout)
 
-from armadillo.armadillio_variables import (credentials, post_login_title,
+from armadillo.armadillo_variables import (credentials, post_login_title,
                                             website, website_title)
 
 
+# Identical to buffalo open_site
 def open_site(browser):
-    pass
+    browser.get(website)
+    if not assert_window_title(browser, website_title):
+        print('Browser failed to successfully open site, please review.')
+        browser.close()
+        quit()
 
 
+# Identical to buffalo open_site
 def locate_login_field(browser, prompt_name, type):
-    pass
+    try:
+        login_prompt_present = EC.presence_of_element_located((By.NAME, prompt_name))
+        WebDriverWait(browser, timeout).until(login_prompt_present)
+        login_prompt = browser.find_element_by_name(prompt_name)
+        return login_prompt
+    except TimeoutException:
+        print(f'Browser timed out trying to locate {type} prompt, please review.')
 
 
 def submit_username(browser):
