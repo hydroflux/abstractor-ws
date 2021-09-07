970b2409c6d4c36b43169accf1783b46bd00f4d3
Jack Hubert <jphubert93@gmail.com>
2021-09-07 15:06:44 -0600

    updated armadillo open_document functions to use the 'Document' class 'number_results' variable instead of passing around the number of results returned; created functions to locate search results table following search verification & began working on a function to open the first result; set up fail safes to pause the program when anything other than 1 results is returned by the search (will address as soon as multiple results are returned)

--- armadillo/open_document.py
+++ armadillo/open_document.py
@@ -32,7 +32,7 @@ def locate_result_count(browser, document):
 def count_results(browser, document):
     result_count = locate_result_count(browser, document)
     if result_count == single_result_message:
-        return 1
+        document.number_results += 1
     else:
         print(f'Browser located multiple search results for '
               f'{extrapolate_document_value(document)}, new logic path needs to be developed.')
@@ -40,7 +40,33 @@ def count_results(browser, document):
         input()
 
 
+def locate_search_results(browser, document):
+    try:
+        search_results_present = EC.presence_of_element_located((By.ID, search_results_id))
+        WebDriverWait(browser, timeout).until(search_results_present)
+        search_results = browser.find_element_by_id(search_results_id)
+        return search_results
+    except TimeoutException:
+        print(f'Browser timed out trying to locate search results for '
+              f'{extrapolate_document_value(document)}, please review.')
+
+
+def open_first_result(browser, document):
+    search_results = locate_search_results(browser, document)
+    print(search_results)
+
+
+def handle_search_results(browser, document):
+    if document.number_results == 1:
+        open_first_result(browser, document)
+    else:
+        print(f'Document instance indicates that more or less than 1 result were located searching '
+              f'{extrapolate_document_value(document)}, please review (should not have reached this stage)')
+        print("Please press enter after reviewing the search parameters...")
+        input()
+
+
 def open_document(browser, document):
     verify_successful_search(browser, document)
-    if count_results == 1:
-        pass
+    count_results(browser, document)
+    handle_search_results(browser, document)
