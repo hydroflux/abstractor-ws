e48fea609b32a2b0203fa5189cf1b468aa03df39
Jack Hubert <jphubert93@gmail.com>
2021-09-08 12:16:12 -0600

    created a 'record' file for armadillo build out with a skeleton function structure to fill out

---
+++ armadillo/record.py
@@ -0,0 +1,52 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def record_grantor(browser, document):
+    pass
+
+
+def record_grantee(browser, document):
+    pass
+
+
+def record_reception_number(browser, document):
+    pass
+
+
+def record_document_type(browser, document):
+    pass
+
+
+def record_document_date(browser, document):
+    pass
+
+
+def record_recording_date(browser, document):
+    pass
+
+
+def record_legal(browser, document):
+    pass
+
+
+def record_related_documents(browser, document):
+    pass
+
+
+def record_comments(browser, document):
+    pass
+
+
+def aggregate_document_information(browser, dataframe, document):
+    pass
+
+
+def record_document_fields(browser, county, dataframe, document):
+    pass
+
+
+def record(browser, county, dataframe, document):
+    pass
