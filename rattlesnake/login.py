1b08504781059e9d3aacc6441b8016940814a3f1
Jack Hubert <jphubert93@gmail.com>
2021-09-13 11:10:17 -0600

    created a 'login' script for rattlesnake & a function skeleton structure to build out the login script

---
+++ rattlesnake/login.py
@@ -0,0 +1,32 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from settings.general_functions import (assert_window_title, fill_search_field, timeout)
+
+from rattlesnake_variables import (credentials, website, website_title)
+
+
+def open_site(browser):
+    pass
+
+
+def locate_login_input():
+    pass
+
+
+def submit_username():
+    pass
+
+
+def submit_password():
+    pass
+
+
+def execute_login():
+    pass
+
+
+def account_login():
+    pass
