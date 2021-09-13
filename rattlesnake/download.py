dc6a5d2d814e6d68fd61dec63343fc4825190035
Jack Hubert <jphubert93@gmail.com>
2021-09-13 16:40:15 -0600

    updated rattlesnake 'download' script to include 'free dowload' and 'add to cart' options

--- rattlesnake/download.py
+++ rattlesnake/download.py
@@ -12,7 +12,7 @@ from settings.file_management import (create_document_directory,
 from settings.general_functions import (center_element, get_direct_link,
                                         newline_split, timeout)
 
-from rattlesnake.rattlesnake_variables import download_page_id, free_download_id, add_to_cart_id
+from rattlesnake.rattlesnake_variables import download_page_id, free_download_button_id, add_to_cart_button_id
 
 
 def locate_download_page(browser, document):
@@ -32,12 +32,26 @@ def open_download_page(browser, document):
     download_page.click()
 
 
-def free_download():
-    pass
+def locate_button(browser, document, id, button_type):
+    try:
+        button_present = EC.element_to_be_clickable((By.ID, id))
+        WebDriverWait(browser, timeout).until(button_present)
+        button = browser.find_element_by_id(id)
+        return button
+    except TimeoutException:
+        print(f'Browser timed out trying to locate "{button_type}" button for '
+              f'{extrapolate_document_value(document)}, please review.')
+        input()
 
 
-def add_to_cart():
-    pass
+def free_download(browser, document):
+    free_download_button = locate_button(browser, document, free_download_button_id, 'free download')
+    free_download_button.click()
+
+
+def add_to_cart(browser, document):
+    add_to_cart_button = locate_button(browser, document, add_to_cart_button_id, 'free download')
+    add_to_cart_button.click()
 
 
 def execute_download():
