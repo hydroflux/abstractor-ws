d31b47a8cf8214781734d2ac6c54e56331956ebd
Jack Hubert <jphubert93@gmail.com>
2021-09-14 15:23:28 -0600

    resorted eagle execute imports

--- eagle/execute.py
+++ eagle/execute.py
@@ -3,7 +3,9 @@ from settings.abstract_object import abstract_dictionary as dataframe
 from settings.bad_search import record_bad_search, unable_to_download
 from settings.driver import create_webdriver
 from settings.export import export_document
-from settings.file_management import (bundle_project, check_length, document_downloaded, document_found, no_document_downloaded,
+from settings.file_management import (bundle_project, check_length,
+                                      document_downloaded, document_found,
+                                      no_document_downloaded,
                                       no_document_found)
 from settings.general_functions import start_timer
 from settings.settings import download
