23d37f307fd12efec00e054f21d9ea1a3c3fea9c
Jack Hubert <jphubert93@gmail.com>
2021-09-15 16:15:28 -0600

    updated armadillo 'open_document' script to use the 'extrapolate_value' class method, added inputs after TimeoutExceptions to pause the program, & created new functions to accss the result link from the new first_result element

--- armadillo/open_document.py
+++ armadillo/open_document.py
@@ -7,7 +7,7 @@ from settings.file_management import extrapolate_document_value
 from settings.general_functions import (get_direct_link, set_description_link,
                                         timeout)
 
-from armadillo.armadillo_variables import (first_result_class_name,
+from armadillo.armadillo_variables import (first_result_class_name, link_tag,
                                            number_results_class,
                                            search_results_id,
                                            single_result_message)
@@ -24,7 +24,8 @@ def locate_result_count(browser, document):
         return result_count
     except TimeoutException:
         print(f'Browser timed out trying to locate result count for '
-              f'{extrapolate_document_value(document)}, please review.')
+              f'{document.extrapolate_value()}, please review.')
+        input()
 
 
 def count_results(browser, document):
@@ -33,7 +34,7 @@ def count_results(browser, document):
         document.number_results += 1
     else:
         print(f'Browser located multiple search results for '
-              f'{extrapolate_document_value(document)}, new logic path needs to be developed.')
+              f'{document.extrapolate_value()}, new logic path needs to be developed.')
         print("Please press enter after reviewing the search results...")
         input()
 
@@ -46,7 +47,8 @@ def locate_search_results(browser, document):
         return search_results
     except TimeoutException:
         print(f'Browser timed out trying to locate search results for '
-              f'{extrapolate_document_value(document)}, please review.')
+              f'{document.extrapolate_value()}, please review.')
+        input()
 
 
 def locate_first_result(search_results, document):
@@ -57,7 +59,8 @@ def locate_first_result(search_results, document):
         return first_result
     except TimeoutException:
         print(f'Browser timed out trying to locate first search result of '
-              f'{extrapolate_document_value(document)}, please review.')
+              f'{document.extrapolate_value()}, please review.')
+        input()
 
 
 def get_first_result(browser, document):
@@ -65,15 +68,32 @@ def get_first_result(browser, document):
     return locate_first_result(search_results, document)
 
 
+def locate_result_link(document, result):
+    try:
+        result_link_present = EC.element_to_be_clickable((By.TAG_NAME, link_tag))
+        WebDriverWait(result, timeout).until(result_link_present)
+        result_link = result.find_element_by_tag_name(link_tag)
+        return result_link
+    except TimeoutException:
+        print(f'Browser timed out trying to locate result link for '
+              f'{document.extrapolate_value()}, please review.')
+        input()
+
+
+def access_result_link(document, result):
+    result_link_element = locate_result_link(document, result)
+    return get_direct_link(result_link_element)
+
+
 def open_result_link(browser, document, result):
     try:
-        document_link = get_direct_link(result)
+        document_link = access_result_link(document, result)
         set_description_link(document, document_link)
         browser.get(document.description_link)
         return True
     except TimeoutException:
         print(f'Browser timed out trying to open result link for '
-              f'{extrapolate_document_value(document)}, please review.')
+              f'{document.extrapolate_value()}, please review.')
         input()
         return False
 
@@ -90,7 +110,7 @@ def handle_search_results(browser, document):
         return open_first_result(browser, document)
     else:
         print(f'Document instance indicates that more or less than 1 result were located searching '
-              f'{extrapolate_document_value(document)}, please review (should not have reached this stage)')
+              f'{document.extrapolate_value()}, please review (should not have reached this stage)')
         print("Please press enter after reviewing the search parameters...")
         input()
         return False
