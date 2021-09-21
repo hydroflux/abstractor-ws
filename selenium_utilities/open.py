f4cf4354c810ba24c99b6f4ef3eb1e4c618be008
Jack Hubert <jphubert93@gmail.com>
2021-09-21 14:52:16 -0600

    created an 'open_url' function in selenium_utilties 'open' in order to begin refactoring other scripts to use the 'open' script functions; began by refactoring rattlesnake 'login'; also added a 'print_failed_window_assertion_statement' function to display a printout to the user when the browser is unable to load the page properly

--- selenium_utilities/open.py
+++ selenium_utilities/open.py
@@ -1,6 +1,19 @@
-def assert_window_title(browser, window_title):
+def assert_window_title(browser, page_title):
     try:
-        assert window_title in browser.title.strip()
+        assert page_title in browser.title.strip()
         return True
     except AssertionError:
         return False
+
+
+def print_failed_window_assertion_statement(page_type, document):
+    if document is None:
+        print(f'Browser failed to successfully open {page_type}, please review.')
+    else:
+        print(f'Browser failed to successfully open {page_type}, please review.')
+
+
+def open_url(browser, url, page_title, page_type, document=None):
+    browser.get(url)
+    if not assert_window_title(browser, page_title):
+        print_failed_window_assertion_statement(page_type, document)
