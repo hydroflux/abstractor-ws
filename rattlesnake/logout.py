a2b63984512beeca1f76d7f8893a062352f08244
Jack Hubert <jphubert93@gmail.com>
2021-09-21 15:27:57 -0600

    refactored rattlesnake 'logout' using selenium_utilities 'inputs' and 'locators' scripts

--- rattlesnake/logout.py
+++ rattlesnake/logout.py
@@ -1,28 +1,10 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from settings.general_functions import timeout
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import locate_element_by_id as locate_button
 
 from rattlesnake.validation import verify_logout
 from rattlesnake.rattlesnake_variables import logout_button_id
 
 
-def locate_logout_button(browser):
-    try:
-        logout_button_present = EC.element_to_be_clickable((By.ID, logout_button_id))
-        WebDriverWait(browser, timeout).until(logout_button_present)
-        logout_button = browser.find_element_by_id(logout_button_id)
-        return logout_button
-    except TimeoutException:
-        print("Browser timed out while trying to locate logout button, please review.")
-
-
-def execute_logout(browser):
-    logout_button = locate_logout_button(browser)
-    logout_button.click()
-
-
 def logout(browser):
-    execute_logout(browser)
+    click_button(browser, locate_button, logout_button_id, "logout button")
     verify_logout(browser)
