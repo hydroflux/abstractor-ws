c9eb478cde7d4f183fbe513e09fcaf1d1f01a2b6
Jack Hubert <jphubert93@gmail.com>
2021-09-23 15:43:28 -0600

    refactored armadillo 'login' to use selenium_utilities 'locators' & 'inputs'

--- armadillo/login.py
+++ armadillo/login.py
@@ -1,11 +1,6 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-
 from selenium_utilities.open import assert_window_title
-
-from settings.general_functions import timeout
+from selenium_utilities.locators import locate_element_by_name as locate_input
+from selenium_utilities.inputs import click_button, enter_input_value
 
 from armadillo.armadillo_variables import credentials, website, website_title
 from armadillo.validation import verify_login
@@ -20,39 +15,15 @@ def open_site(browser):
         quit()
 
 
-# Identical to buffalo locate_login_input
-def locate_login_input(browser, input_name, type):
-    try:
-        login_prompt_present = EC.presence_of_element_located((By.NAME, input_name))
-        WebDriverWait(browser, timeout).until(login_prompt_present)
-        login_prompt = browser.find_element_by_name(input_name)
-        return login_prompt
-    except TimeoutException:
-        print(f'Browser timed out trying to locate {type} input, please review.')
-
-
-# Matches buffalo submit_username
-def submit_username(browser):
-    fill_search_field(
-        locate_login_input(browser, credentials[0], "username"),
-        credentials[1])
-
-
-# Matches buffalo submit_password
-def submit_password(browser):
-    fill_search_field(
-        locate_login_input(browser, credentials[2], "password"),
-        credentials[3])
-
-
-def execute_login(browser):
-    submit_button = locate_login_input(browser, credentials[4], 'submit')
-    submit_button.click()
+def enter_credentials(browser):
+    enter_input_value(browser, locate_input, credentials[0],  # Submit Username
+                      "username input", credentials[1])
+    enter_input_value(browser, locate_input, credentials[2],  # Submit Password
+                      "password input", credentials[3])
+    click_button(browser, locate_input, credentials[4], 'submit')  # Click Login Button    
 
 
 def account_login(browser):
     open_site(browser)
-    submit_username(browser)
-    submit_password(browser)
-    execute_login(browser)
+    enter_credentials(browser)
     verify_login(browser)
