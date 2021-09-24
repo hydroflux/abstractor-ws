1791746af3be98bc5298077d33b0b7282855863d
Jack Hubert <jphubert93@gmail.com>
2021-09-24 15:29:22 -0600

    created a page selector for 'download_early_documents' script in rattlesnake

--- rattlesnake/download_early_documents.py
+++ rattlesnake/download_early_documents.py
@@ -1,17 +1,18 @@
+from selenium.webdriver.support.ui import Select
 from settings.user_prompts import clear_terminal
 from selenium_utilities.inputs import click_button
 from rattlesnake.search import clear_search, handle_document_value_numbers
 from selenium_utilities.open import assert_window_title, open_url
-from selenium_utilities.locators import locate_element_by_id as locate_input
+from selenium_utilities.locators import locate_element_by_id as locate_element
 
-from rattlesnake.rattlesnake_variables import early_search_url, early_search_title, early_document_image_title
+from rattlesnake.rattlesnake_variables import early_search_url, early_search_title, early_document_image_title, page_selector_id
 
 
 def search_early_document(browser, document):
     handle_document_value_numbers(browser, document)
-    click_button(browser, locate_input, document.input_ids["Volume"],
+    click_button(browser, locate_element, document.input_ids["Volume"],
                  "Volume Input", document)
-    click_button(browser, locate_input, document.button_ids["Submit Button"],
+    click_button(browser, locate_element, document.button_ids["Submit Button"],
                  "submit button", document)  # Execute Search
 
 
@@ -35,11 +36,23 @@ def check_document_image_page(browser):
         return handle_document_image_page_user_input()
 
 
+def go_to_page(browser, document, page_value):
+    page_selector = Select(locate_element(browser, page_selector_id, "page selector", True, document))
+    page_selector.selectByValue(page_value)
+
+
+def download_early_document_image(browser, document, target_directory, next_page=True):
+    page_value = int(document.document_value()[1] - 1)
+    while next_page is True:
+        go_to_page(browser, document, page_value)
+
+
 def download_early_documents(browser, target_directory, document_list):
     for document in document_list:
+        clear_terminal()
         print(f'Now searching {document.document_value()}...')
         open_url(browser, early_search_url, early_search_title, "old document search")
         clear_search(browser, document)
         search_early_document(browser, document)
         if check_document_image_page(browser):
-            pass
+            download_early_document_image(browser, document, target_directory)
