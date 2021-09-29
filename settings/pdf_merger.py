02b949ff5d9efffa127c5b1e012f870d5c620a2c
Jack Hubert <jphubert93@gmail.com>
2021-09-28 20:55:20 -0600

    updated 'pdf_merger' after updating 'sort_pdf_dictionary'

--- settings/pdf_merger.py
+++ settings/pdf_merger.py
@@ -3,20 +3,21 @@ import os
 
 
 def sort_pdf_dictionary(dictionary):
-    return [{key: sorted(value)} for key, value in dictionary.items()]
+    return {key: sorted(value)[-1:] + sorted(value)[:-1] for key, value in dictionary.items()}
 
 
 def create_pdf_dictionary(target_directory, pdf_dictionary={}):
     for pdf in os.listdir(target_directory):
-        pdf_name = f'{pdf[:15]}.pdf'
-        if pdf_name in pdf_dictionary:
-            pdf_dictionary[pdf_name].append(pdf)
-        else:
-            pdf_dictionary[pdf_name] = [pdf]
+        if not pdf.endswith('.DS_Store'):
+            pdf_name = f'{pdf[:15]}.pdf'
+            if pdf_name in pdf_dictionary:
+                pdf_dictionary[pdf_name].append(pdf)
+            else:
+                pdf_dictionary[pdf_name] = [pdf]
     return sort_pdf_dictionary(pdf_dictionary)
 
 
 def rename_pdfs(target_directory):
     pdf_dictionary = create_pdf_dictionary(target_directory)
-    for item in pdf_dictionary.items():
-        print(item)
+    for name, pdfs in pdf_dictionary.items():
+        print(pdfs)
