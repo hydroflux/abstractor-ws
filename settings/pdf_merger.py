cc90eeba63fe49728895f0c4dd6515620d8d3bc9
Jack Hubert <jphubert93@gmail.com>
2021-09-28 21:21:24 -0600

    finished 'pdf_merger' script

--- settings/pdf_merger.py
+++ settings/pdf_merger.py
@@ -17,11 +17,22 @@ def create_pdf_dictionary(target_directory, pdf_dictionary={}):
     return sort_pdf_dictionary(pdf_dictionary)
 
 
-def rename_pdfs(target_directory):
-    os.chdir(target_directory)
-    pdf_dictionary = create_pdf_dictionary(target_directory)
+def merge_pdfs(target_directory, pdf_dictionary):
     for name, pdfs in pdf_dictionary.items():
         merger = PdfFileMerger()
         [merger.append(PdfFileReader(f'{target_directory}/{pdf}', 'rb')) for pdf in pdfs]
         merger.write(f'{name}')
         merger.close()
+
+
+def remove_original_files(target_directory):
+    for file in os.listdir(target_directory):
+        if len(file) > 19 or file.endswith('.DS_Store'):
+            os.remove(file)
+
+
+def rename_pdfs(target_directory):
+    os.chdir(target_directory)
+    pdf_dictionary = create_pdf_dictionary(target_directory)
+    merge_pdfs(target_directory, pdf_dictionary)
+    remove_original_files(target_directory)
