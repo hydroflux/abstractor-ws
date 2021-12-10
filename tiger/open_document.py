bc5174e82beb1faeac5654f3a9cabd0c3592e1d1
Jack Hubert <jphubert93@gmail.com>
2021-12-10 11:47:25 -0700

    changed 'get_first_row' to 'access_first_row' & 'get_results_table_body' to 'access_results_table'; eliminated 'identify_first_result' function & updated 'check_result' to 'check_results'

--- tiger/open_document.py
+++ tiger/open_document.py
@@ -1,18 +1,14 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium_utilities.element_interaction import center_element
-
+from selenium_utilities.element_interaction import (center_element,
+                                                    get_element_text)
 from selenium_utilities.inputs import click_button
-from selenium_utilities.locators import locate_element_by_id, locate_element_by_tag_name, locate_elements_by_tag_name
-from selenium_utilities.element_interaction import get_element_text
+from selenium_utilities.locators import (locate_element_by_id,
+                                         locate_element_by_tag_name,
+                                         locate_elements_by_tag_name)
 
 from settings.county_variables.tiger import (first_result_tag, result_cell_tag,
                                              result_count_button_id,
                                              result_count_id, results_body_tag,
                                              results_id)
-from settings.file_management import document_value, extrapolate_document_value
 
 
 def count_results(browser, document):
@@ -23,7 +19,7 @@ def count_results(browser, document):
     return result_count.text.split(' ')[-1]
 
 
-def get_results_table_body(browser, document):
+def access_results_table(browser, document):
     results = locate_element_by_id(browser, results_id,
                                    "results section", document=document)
     center_element(browser, results)
@@ -32,19 +28,14 @@ def get_results_table_body(browser, document):
     return results_table
 
 
-def get_first_row(results_table_body, document):
+def access_first_row(results_table_body, document):
     all_results = locate_elements_by_tag_name(results_table_body, first_result_tag,
                                               "search results", document=document)
     return all_results[0]
 
 
-def identify_first_result(browser, document):
-    results_table_body = get_results_table_body(browser, document)
-    return get_first_row(browser, results_table_body, document)
-
-
-def check_result(browser, document):
-    first_result = identify_first_result(browser, document)
+def check_result(results_table, document):
+    first_result = access_first_row(results_table, document)
     first_result_cells = first_result.find_elements_by_tag_name(result_cell_tag)
     if document.value() in map(get_element_text, first_result_cells):
         return True
@@ -52,6 +43,7 @@ def check_result(browser, document):
 
 def open_document(browser, document):
     count_results(browser, document)
-    if check_result(browser, document):
-        identify_first_result(browser, document).click()
+    results_table = access_results_table(browser, document)
+    if check_result(results_table, document):
+        access_first_row(results_table, document).click()
         return True
