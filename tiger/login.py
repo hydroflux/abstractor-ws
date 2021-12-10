f796764a977aec1bac13c66fd7129860d69fb44e
Jack Hubert <jphubert93@gmail.com>
2021-12-10 10:44:28 -0700

    dropped the 'handle_disclaimer' function in favor of using the 'click_button' function instead; updated 'login' imports in order to remove selenium webdriver imports

--- tiger/login.py
+++ tiger/login.py
@@ -1,8 +1,3 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium_utilities.inputs import click_button, enter_input_value
 from selenium_utilities.locators import (locate_element_by_id,
                                          locate_element_by_name)
@@ -13,7 +8,6 @@ if __name__ == '__main__':
                                                  handle_disclaimer_id,
                                                  login_button_name, website,
                                                  website_title)
-    from settings.general_functions import timeout
 
     from tiger.search import open_search
 else:
@@ -21,7 +15,6 @@ else:
                                                    handle_disclaimer_id,
                                                    login_button_name, website,
                                                    website_title)
-    from ..settings.general_functions import timeout
     from .search import open_search
 
 
@@ -31,18 +24,9 @@ def enter_credentials(browser):
     click_button(browser, locate_element_by_name, login_button_name, "login button")
 
 
-def handle_disclaimer(browser):
-    try:
-        disclaimer_present = EC.element_to_be_clickable((By.ID, handle_disclaimer_id))
-        WebDriverWait(browser, timeout).until(disclaimer_present)
-        disclaimer = browser.find_element_by_id(handle_disclaimer_id)
-        disclaimer.click()
-    except TimeoutException:
-        print("Browser timed out while trying to handle the website disclaimer.")
-
-
 def account_login(browser):
     open_url(browser, website, website_title, "county site")
     enter_credentials(browser)
     open_search(browser)
-    handle_disclaimer(browser)
+    click_button(browser, locate_element_by_id,
+                 handle_disclaimer_id, "login disclaimer")  # Handle Disclaimer
