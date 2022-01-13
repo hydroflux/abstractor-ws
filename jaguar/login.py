59298220f8c98b6134bee04a6771767a80626fd4
Jack Hubert <jphubert93@gmail.com>
2022-01-12 17:25:12 -0700

    updated the jaguar 'login' script with an 'account_login' function and appropriate imports

---
+++ jaguar/login.py
@@ -0,0 +1,10 @@
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import locate_element_by_name
+from selenium_utilities.open import open_url
+
+from settings.county_variables.jaguar import home_page_title, home_page_url, login_button_name
+
+
+def account_login(browser):
+    open_url(browser, home_page_url, home_page_title, "county site")
+    click_button(browser, locate_element_by_name, login_button_name, "login button")
