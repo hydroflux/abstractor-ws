9dc9fbc0ec3feb2d42252120a78d86460c40b4df
Jack Hubert <jphubert93@gmail.com>
2022-01-12 18:54:58 -0700

    updated 'validate_search' function

--- jaguar/open_document.py
+++ jaguar/open_document.py
@@ -1,4 +1,4 @@
-from jaguar.validation import verify_search_results_page_loaded, verify_results_loaded
+from jaguar.validation import validate_search, verify_results_loaded
 
 
 def count_results(browser, document):
@@ -10,7 +10,7 @@ def handle_search_results(browser, document):
 
 
 def open_document(browser, document):
-    verify_search_results_page_loaded(browser, document)
+    validate_search(browser, document)
     if verify_results_loaded(browser, document):
         count_results(browser, document)
         return handle_search_results(browser, document)
--- jaguar/validation.py
+++ jaguar/validation.py
@@ -1,5 +1,20 @@
-def verify_search_results_page_loaded(browser, document):
-    pass
+from settings.county_variables.jaguar import search_results_title, validation_class_name, invalid_search_message
+from selenium_utilities.open import assert_window_title
+from selenium_utilities.locators import locate_element_by_class_name
+
+
+def validate_search(browser, document):
+    if not assert_window_title(browser, search_results_title):
+        print(f'Browser failed to successfully execute search for '
+              f'"{document.extrapolate_value()}", validating search...')
+        error_message_element = locate_element_by_class_name(browser, validation_class_name,
+                                                             "search results", False, document)
+        if error_message_element.text.strip() == invalid_search_message:
+            print(invalid_search_message)
+        else:
+            print('Unable to identify the issue, please review.')
+            print(error_message_element.text.strip())
+        print('Please review and press enter to continue...')
 
 
 def verify_results_loaded(browser, document):
