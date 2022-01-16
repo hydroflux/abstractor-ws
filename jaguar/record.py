503590eed867e5bb3f70ab6c09926010ab893402
Jack Hubert <jphubert93@gmail.com>
2022-01-16 15:24:23 -0700

    updated jaguar 'record' scaffold

--- jaguar/record.py
+++ jaguar/record.py
@@ -1,8 +1,19 @@
-def record_document_fields(browser, dataframe, document):
+def record_document_type_and_number(browser, dataframe, document):
+    pass
+
+
+def aggregate_document_table_information(browser, dataframe, document):
+    pass
+
+
+def record_comments(dataframe, document):
+    pass
+
+
+def record_document_link(dataframe, document):
     pass
 
 
 def record(browser, dataframe, document):
-    pass
-    # verify
-    # record
+    record_document_type_and_number(browser, dataframe, document)
+    aggregate_document_table_information(browser, dataframe, document)
