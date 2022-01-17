1b2b51413b3bbf9256d1315ece19093f342ccabe
Jack Hubert <jphubert93@gmail.com>
2022-01-17 11:38:58 -0700

    updated jaguar 'execute' imports; filled out hte 'execute_download' function by pulling in the 'update_download' and 'click_button' functions from different modules

--- jaguar/download.py
+++ jaguar/download.py
@@ -1,8 +1,22 @@
-from settings.download_management import previously_downloaded
+import os
+
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import locate_element_by_class_name
+
+from settings.download_management import previously_downloaded, update_download
+from settings.county_variables.jaguar import download_button_class
 
 
 def execute_download(browser, document_directory, document):
-    pass
+    document.download_value = f'{document.reception_number}.pdf'
+    number_files = len(os.listdir(document_directory))
+    click_button(browser, locate_element_by_class_name, download_button_class, "download button", document)
+    return update_download(
+        browser,
+        document_directory,
+        document,
+        number_files
+    )
 
 
 def download_document(browser, document_directory, document):
--- jaguar/execute.py
+++ jaguar/execute.py
@@ -1,15 +1,19 @@
-from jaguar.record import record
 from settings.abstract_object import abstract_dictionary as dataframe
 from settings.bad_search import record_bad_search, unable_to_download
 from settings.driver import create_webdriver
 from settings.export import export_document
-from settings.file_management import bundle_project, create_document_directory, document_downloaded, document_found, no_document_downloaded, no_document_found
+from settings.file_management import (bundle_project,
+                                      create_document_directory,
+                                      document_downloaded, document_found,
+                                      no_document_downloaded,
+                                      no_document_found)
 from settings.general_functions import start_timer
 from settings.settings import download
 
+from jaguar.download import download_document
 from jaguar.login import account_login
 from jaguar.open_document import open_document
-from jaguar.download import download_document
+from jaguar.record import record
 from jaguar.search import search
 from jaguar.transform import transform_document_list
 
