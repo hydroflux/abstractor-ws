9e58b1eeb4e91b9154c1deff5df86dd9b935e23c
Jack Hubert <jphubert93@gmail.com>
2022-01-19 16:14:10 -0700

    updated 'test_dataframe'

---
+++ settings/objects/test_dataframe.py
@@ -0,0 +1,62 @@
+test_dictionary = {
+    "Grantor":
+        [
+            'Odinson, Loki',
+            'Odinson, Thor',
+            'No Record Found',
+        ],
+    "Grantee":
+        [
+            'Borson, Odin',
+            'Borson, Odin',
+            'No Record Found',
+        ],
+    "Book":
+        [
+            '999',
+            '1029',
+            'N/A',
+        ],
+    "Page":
+        [
+            '898',
+            '4',
+            'N/A',
+        ],
+    "Reception Number":
+        [
+            '',
+            '',
+            '',
+        ],
+    "Document Type":
+        [
+            'Birthright',
+            'Birthright',
+            'No Record Found',
+        ],
+    "Recording Date":
+        [
+            '12/01/2021',
+            '12/15/2021',
+            '01/01/1900',
+        ],
+    "Legal":
+        [
+            '',
+            '',
+            'N/A',
+        ],
+    "Related Documents":
+        [
+            '',
+            '',
+            '',
+        ],
+    "Comments":
+        [
+            '',
+            'Multiple documents located',
+            'No document located',
+        ]
+}
