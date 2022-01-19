c9d30ffebcdbdcc8774fb96c084b88ba793dbae4
Jack Hubert <jphubert93@gmail.com>
2022-01-19 16:06:20 -0700

    updated the Abstract  class syntax around the 'review' trigger

--- settings/classes/Abstract.py
+++ settings/classes/Abstract.py
@@ -1,8 +1,8 @@
 class Abstract:
-    def __init__(self, county, target_directory, download, review,
+    def __init__(self, county, target_directory, download, review=False,
                  document_list=None):
         self.county = county
         self.target_directory = target_directory
         self.download = download
-        self.review = False
+        self.review = review
         self.document_list = document_list
