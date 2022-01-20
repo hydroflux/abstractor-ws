a7355444463b4b32d9155b31e73f0f0c26d4942e
Jack Hubert <jphubert93@gmail.com>
2022-01-20 11:15:26 -0700

    updated 'conditional_formatting' functions to access the correct font format in each helper function, rather than passing at the function call

--- settings/export/conditional_formatting.py
+++ settings/export/conditional_formatting.py
@@ -1,29 +1,33 @@
-def add_no_record_format(project, font_format):
+def add_no_record_format(project):
+    font_format = project.font_formats['no_record']
     no_record_format = project.worksheet_properties['conditional_formats']['no_record_format']
     no_record_format['format'] = font_format
     project.worksheet.conditional_format(project.worksheet_range, no_record_format)
 
 
-def add_multiple_document_format(project, font_format):
+def add_multiple_document_format(project):
+    font_format = project.font_formats['multiple_documents']
     multi_documents_format = project.worksheet_properties['conditional_formats']['multi_documents_format']
     multi_documents_format['format'] = font_format
     project.worksheet.conditional_format(project.worksheet_range, multi_documents_format)
 
 
-def add_no_document_image_format(project, font_format):
+def add_no_document_image_format(project):
+    font_format = project.font_formats['no_image']
     no_document_image_format = project.worksheet_properties['conditional_formats']['no_image_format']
     no_document_image_format['format'] = font_format
     project.worksheet.conditional_format(project.worksheet_range, no_document_image_format)
 
 
-def add_out_of_county_format(project, font_format):
+def add_out_of_county_format(project):
+    font_format = project.font_formats['out_of_county']
     out_of_county_format = project.worksheet_properties['conditional_formats']['out_of_county_format']
     out_of_county_format['format'] = font_format
     project.worksheet.conditional_format(project.worksheet_range, out_of_county_format)
 
 
 def add_conditional_formatting(project):
-    add_no_record_format(project, project.font_formats['no_record'])
-    add_multiple_document_format(project, project.font_formats['multiple_documents'])
-    add_no_document_image_format(project, project.font_formats['no_image'])
-    add_out_of_county_format(project, project.font_formats['out_of_county'])
+    add_no_record_format(project)
+    add_multiple_document_format(project)
+    add_no_document_image_format(project)
+    add_out_of_county_format(project)
