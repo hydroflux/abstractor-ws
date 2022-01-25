0d59e44960b10b403ce8c9d89683d0c4db60e90c
Jack Hubert <jphubert93@gmail.com>
2022-01-25 08:32:25 -0700

    updated leopard 'download' script imports; further built out the 'execute_download' function by extrapolating some of the logic from the 'download_document' function into 'execute_download' using the eagle download script 'execute_download' function as a guide; eliminated the 'open_document_submenu' function

--- engines/leopard/download.py
+++ engines/leopard/download.py
@@ -1,16 +1,11 @@
 import os
 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium_utilities.inputs import click_button
 from selenium_utilities.locators import locate_element_by_id
 
 from settings.county_variables.leopard import (download_button_id,
                                                stock_download, view_group_id)
 from settings.download_management import previously_downloaded, update_download
-from settings.general_functions import timeout
 from settings.initialization import create_document_directory
 from settings.invalid import no_document_image
 
@@ -18,12 +13,10 @@ from settings.invalid import no_document_image
 print("download", __name__)
 
 
-def open_download_submenu(browser, document):
-    click_button(browser, locate_element_by_id, view_group_id, "download submenu",)
-
-
 def execute_download(browser, document):
-    click_button(browser, locate_element_by_id, download_button_id, "download button", document)
+    number_files = len(os.listdir(document_directory))
+    click_button(browser, locate_element_by_id, view_group_id, "download submenu", document)  # Open Download Submenu
+    click_button(browser, locate_element_by_id, download_button_id, "download button", document)  # Download Document
 
 
 def download_document(browser, abstract, document):
@@ -31,8 +24,6 @@ def download_document(browser, abstract, document):
     if previously_downloaded(county, document_directory, document_number):
         return True
     else:
-        number_files = len(os.listdir(document_directory))
-        open_download_submenu(browser, document)
         execute_download(browser, document)
         if update_download(browser, county, stock_download, document_directory, number_files, document_number):
             return True
