bcae3e23d9abd76859ab29b3e2e9837bea806c8f
Jack Hubert <jphubert93@gmail.com>
2022-01-26 09:54:02 -0700

    updated function syntax of 'execute_download' and 'download_document' in the jaguar 'download' script

--- engines/jaguar/download.py
+++ engines/jaguar/download.py
@@ -10,6 +10,7 @@ from settings.county_variables.jaguar import download_button_class
 from settings.file_management import document_downloaded
 
 
+# Nearly identical to 'leopard' prepare_for_download
 def prepare_for_download(abstract, document):
     abstract.document_directory = create_document_directory(abstract.target_directory)
     abstract.document_directory_files = len(os.listdir(abstract.document_directory))
@@ -18,17 +19,12 @@ def prepare_for_download(abstract, document):
 
 # Very similar but not identical to 'leopard' execute_download
 def execute_download(browser, abstract, document):
-    click_button(browser, locate_element_by_class_name, download_button_class, "download button", document)
-    update_download(
-        browser,
-        abstract,
-        document
-    )
+    click_button(browser, locate_element_by_class_name,  # Download Document
+                 download_button_class, "download button", document)
+    update_download(browser, abstract, document)
 
 
 def download_document(browser, abstract, document):
     prepare_for_download(abstract, document)
-    if previously_downloaded(abstract, document):
-        return True
-    else:
+    if not previously_downloaded(abstract, document):
         execute_download(browser, abstract, document)
