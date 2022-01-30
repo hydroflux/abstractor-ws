dd381840af353d170eed392e1366c5a513d893d4
Jack Hubert <jphubert93@gmail.com>
2022-01-30 13:22:09 -0700

    created a 'download_document' function in the 'downloader' action script to extrapolate value across the disparate 'download' scripts in different county engines

--- actions/downloader.py
+++ actions/downloader.py
@@ -1,4 +1,5 @@
 import os
+from settings.download_management import previously_downloaded
 
 from settings.initialization import create_folder
 
@@ -15,3 +16,9 @@ def prepare_for_download(abstract, document):
     abstract.document_directory_files = len(os.listdir(abstract.document_directory))
     if abstract.county.engine == 'jaguar':
         document.download_value = f'{document.reception_number}.pdf'
+
+
+def download_document(browser, abstract, document, execute_download):
+    prepare_for_download(abstract, document)
+    if not previously_downloaded(abstract, document):
+        execute_download(browser, abstract, document)
