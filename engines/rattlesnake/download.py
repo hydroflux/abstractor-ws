d7e478db5c58f92c59da5372e7d521df1581fc24
Jack Hubert <jphubert93@gmail.com>
2022-01-30 13:19:30 -0700

    updated the script imports for rattlesnake 'download' script

--- engines/rattlesnake/download.py
+++ engines/rattlesnake/download.py
@@ -1,11 +1,5 @@
-import os
-
 from actions.downloader import prepare_for_download
 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium_utilities.inputs import click_button
 from selenium_utilities.locators import locate_element_by_id
 
@@ -13,10 +7,9 @@ from settings.county_variables.rattlesnake import (add_to_cart_button_id,
                                                    download_page_id,
                                                    free_download_button_id)
 from settings.download_management import previously_downloaded, update_download
-from settings.general_functions import timeout
 
 from engines.rattlesnake.validation import (verify_document_image_page_loaded,
-                                    verify_valid_download)
+                                            verify_valid_download)
 
 
 def handle_document_download_type(browser, abstract, document):
@@ -26,7 +19,7 @@ def handle_document_download_type(browser, abstract, document):
         if verify_valid_download(browser):
             update_download(browser, abstract, document)
     elif document.download_type == 'paid':
-        click_button(browser, locate_element_by_id, free_download_button_id,
+        click_button(browser, locate_element_by_id, add_to_cart_button_id,
                      "add to cart button", document)  # Click Add To Cart Button
 
 
