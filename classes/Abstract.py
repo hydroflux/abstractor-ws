bee5c378fd9da7fd4364fd66bc701cb0921e8d23
Jack Hubert <jphubert93@gmail.com>
2022-01-31 10:41:01 -0700

    updated Abstract instance methods 'account_for_number_results', 'remaining_documents', and 'report_execution_time' with the 'self' parameter

--- classes/Abstract.py
+++ classes/Abstract.py
@@ -96,19 +96,19 @@ class Abstract:
     def last_document(self, document):
         return self.document_list[self.document_list.index(document) - 1]
 
-    def account_for_number_results(document):
+    def account_for_number_results(self, document):
         if document.number_results > 1:
             return f' - {document.number_results} results found for {document.extrapolate_value()}'
         else:
             return ''
 
-    def remaining_documents(document_list, document):
-        return len(document_list) - document_list.index(document) - 1
+    def remaining_documents(self, document):
+        return len(self.document_list) - self.document_list.index(document) - 1
 
     def list_remaining_documents(self, document):
         return f'{self.remaining_documents(document)} documents remaining'
 
-    def report_execution_time(time):
+    def report_execution_time(self, time):
         return str(datetime.now() - time)
 
     def document_found(self, document):
