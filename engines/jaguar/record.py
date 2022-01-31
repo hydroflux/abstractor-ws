0095638002b5c52c36b03bfbaf0fed2926d93d33
Jack Hubert <jphubert93@gmail.com>
2022-01-31 11:08:00 -0700

    updated the 'record_document_type_and_number' and 'handle_reception_number' functions in the jaguar 'record' script to use the current Abstract instance

--- engines/jaguar/record.py
+++ engines/jaguar/record.py
@@ -23,21 +23,20 @@ def access_document_type_and_number(browser, document):
     return document_type_and_number.split(' - ')
 
 
-def handle_reception_number(dataframe, document, reception_number):
-    if validate_reception_number(document, reception_number):
-        document.reception_number = reception_number
-        dataframe['Reception Number'].append(reception_number)
+def handle_reception_number(dataframe, document):
+    if validate_reception_number(document):
+        dataframe['Reception Number'].append(document.reception_number)
     else:
-        print(f'Reception number "{reception_number}" does not match the expected value for '
+        print(f'Reception number "{document.reception_number}" does not match the expected value for '
               f'{document.extrapolate_value()}, please review and press enter to continue...')
         input()
 
 
-def record_document_type_and_number(browser, dataframe, document):
+def record_document_type_and_number(browser, abstract, document):
     document_type, reception_number = access_document_type_and_number(browser, document)
     document.reception_number = reception_number
-    dataframe['Document Type'].append(update_sentence_case_extras(title_strip(document_type)))
-    handle_reception_number(dataframe, document, reception_number)
+    abstract.dataframe['Document Type'].append(update_sentence_case_extras(title_strip(document_type)))
+    handle_reception_number(abstract, document)
 
 
 def record_indexing_information(abstract, document_table, document):
@@ -92,7 +91,7 @@ def record_document_link(dataframe, document):
 
 
 def record(browser, abstract, document):
-    record_document_type_and_number(browser, abstract.dataframe, document)
+    record_document_type_and_number(browser, abstract, document)
     aggregate_document_table_information(browser, abstract, document)
     record_comments(abstract, document)
     record_document_link(abstract.dataframe, document)
