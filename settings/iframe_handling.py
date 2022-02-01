3ee079ff0dfe8af1ea3936774468ee069d99b19d
Jack Hubert <jphubert93@gmail.com>
2022-02-01 10:30:35 -0700

    dropped the 'locate_iframe_by_name' function from the 'iframe_handling' script; updated imports

--- settings/iframe_handling.py
+++ settings/iframe_handling.py
@@ -1,11 +1,4 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-
 from selenium_utilities.locators import locate_elements_by_tag_name
-
-from project_management.timers import timeout
 
 
 def access_iframe_by_tag(browser):
@@ -17,16 +10,6 @@ def access_iframe_by_tag(browser):
               'please attempt to access using a different method.')
     else:
         return iframes[0]
-
-
-def locate_iframe_by_name(browser, iframe_name):
-    try:
-        iframe_present = EC.presence_of_element_located((By.NAME, iframe_name))
-        WebDriverWait(browser, timeout).until(iframe_present)
-        iframe = browser.find_element_by_name(iframe_name)
-        return iframe
-    except TimeoutException:
-        print(f'Browser timed out trying to locate {iframe_name}, please review.')
 
 
 def switch_to_default_content(browser):
