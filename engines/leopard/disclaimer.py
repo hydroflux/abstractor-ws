479629384b5306002eeef48f7accbed6d5bbc4f5
Jack Hubert <jphubert93@gmail.com>
2022-02-01 10:47:10 -0700

    dropped the 'locate_disclaimer' function from the leopard 'disclaimer' script in and replaced with the 'locate_element_by_id' function from the 'locators' script; updated imports accordingly

--- engines/leopard/disclaimer.py
+++ engines/leopard/disclaimer.py
@@ -1,9 +1,5 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from project_management.timers import naptime
 
-from project_management.timers import naptime, timeout
 from selenium_utilities.locators import locate_element_by_id
 
 from settings.county_variables.leopard import (disclaimer_active_class,
@@ -12,19 +8,9 @@ from settings.county_variables.leopard import (disclaimer_active_class,
 from settings.general_functions import javascript_script_execution
 
 
-def locate_disclaimer(browser):
-    try:
-        disclaimer_present = EC.presence_of_element_located((By.ID, disclaimer_id))
-        WebDriverWait(browser, timeout).until(disclaimer_present)
-        disclaimer = browser.find_element_by_id(disclaimer_id)
-        return disclaimer
-    except TimeoutException:
-        print("Browser timed out while trying to locate disclaimer while logging in, please review.")
-
-
 def handle_disclaimer(browser):
     javascript_script_execution(browser, open_script)
-    disclaimer = locate_disclaimer(browser)
+    disclaimer = locate_element_by_id(browser, disclaimer_id, "disclaimer", False)
     if disclaimer.get_attribute('class') == disclaimer_active_class:
         disclaimer_button = locate_element_by_id(browser, disclaimer_button_id, "disclaimer button", True)
         disclaimer_button.click()
