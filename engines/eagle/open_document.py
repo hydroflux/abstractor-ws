7d194cd7cbecd5ef1ade52d510a48c0091e2d4b5
Jack Hubert <jphubert93@gmail.com>
2022-02-02 13:07:42 -0700

    dropped commented out functions in the eagle 'open_document' script

--- engines/eagle/open_document.py
+++ engines/eagle/open_document.py
@@ -74,16 +74,6 @@ def retry_execute_search(browser, document, search_status):
     return search_status
 
 
-# def locate_first_result(browser):
-#     try:
-#         first_result_present = EC.element_to_be_clickable((By.CLASS_NAME, search_result_class_name))
-#         WebDriverWait(browser, timeout).until(first_result_present)
-#         first_result = browser.find_elements_by_class_name(search_result_class_name)
-#         return first_result
-#     except TimeoutException:
-#         print("Browser timed out trying to retrieve the first result of the search.")
-
-
 def get_search_results(browser, document):
     result_rows = locate_elements_by_class_name(browser, results_row_class_name,
                                                 "search results", True, document=document)
@@ -119,15 +109,6 @@ def handle_result_count(browser, document):
         count_results(browser, document)
 
 
-# def process_result_count_from_search(browser, document):
-#     while document.number_results is None:
-#         print(f'Result count returned "None" for '
-#               f'{document.extrapolate_value()}, attempting to execute search again.')
-#         retry_search(browser, document)  # Should correct for TypeError; doesn't account for fill_search_field failing
-#         result_count = handle_result_count(browser, document)
-#     return result_count
-
-
 def check_search_results(browser, document):
     handle_result_count(browser, document)
     # number_results = process_result_count_from_search(browser, document)
@@ -137,16 +118,6 @@ def check_search_results(browser, document):
         if document.number_results > 1:
             print(f'{document.number_results} documents returned while searching {document.extrapolate_value()}.')
         return True
-
-
-# def view_search_actions(browser, result):
-#     try:
-#         search_actions_list_present = EC.presence_of_element_located((By.CLASS_NAME, search_actions_class_name))
-#         WebDriverWait(browser, timeout).until(search_actions_list_present)
-#         search_actions_list = result.find_element_by_class_name(search_actions_class_name)
-#         return search_actions_list.find_elements_by_tag_name(search_action_tag_name)
-#     except TimeoutException:
-#         print("Browser timed out while trying to access search actions.")
 
 
 def get_document_link(result, document):
@@ -159,7 +130,8 @@ def get_document_link(result, document):
 def open_document_description(browser, document, result):
     document_link = get_document_link(result, document)
     document.description_link = get_direct_link(document_link)
-    open_url(browser, document.description_link, document_description_title, "document description", document)
+    open_url(browser, document.description_link, document_description_title,
+             "document description", document)
 
 
 def handle_document_search(browser, document):
