7405fd17b29925cbb88d85e34945bcd5fc9850b2
Jack Hubert <jphubert93@gmail.com>
2022-02-03 14:45:34 -0700

    dropped the 'locate_logout_button' function from leopard 'logout' in favor of using the 'locate_element_by_id' function from the 'locators' script; changed 'get_logout_link' to 'access_logout_link'

--- engines/leopard/logout.py
+++ engines/leopard/logout.py
@@ -1,31 +1,12 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-
-from project_management.timers import timeout
-
-# Use the following print statement to identify the best way to manage imports for Django vs the script folder
-print("logout", __name__)
+from selenium_utilities.locators import locate_element_by_id
 
 
-def locate_logout_button(browser, abstract):
-    try:
-        logout_button_present = EC.presence_of_element_located((By.ID, abstract.county.buttons["Logout"]))
-        # Element to be clickable seems to make more sense, need to investigate the UI when
-        # updating leopard again
-        WebDriverWait(browser, timeout).until(logout_button_present)
-        logout_button = browser.find_element_by_id(abstract.county.buttons["Logout"])
-        return logout_button
-    except TimeoutException:
-        print("Browser timed out while trying to locate logout button, please review.")
-
-
-def get_logout_link(browser, abstract):
-    logout_button = locate_logout_button(browser, abstract)
+def access_logout_link(browser, abstract):
+    logout_button = locate_element_by_id(browser, abstract.county.buttons["Logout"],
+                                         "logout button", True)
     return logout_button.get_attribute("href")
 
 
 def logout(browser, abstract):
-    logout_link = get_logout_link(browser, abstract)
+    logout_link = access_logout_link(browser, abstract)
     browser.get(logout_link)
