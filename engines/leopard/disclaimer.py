468cb858f297beeb045ede6023f00697aa9fe6c6
Jack Hubert <jphubert93@gmail.com>
2022-02-03 15:00:00 -0700

    dropped all leopard settings imports from the 'disclaimer' script and updated with Abstract County attributes; updated syntax to make 'handle_disclaimer' function easier to read

--- engines/leopard/disclaimer.py
+++ engines/leopard/disclaimer.py
@@ -3,15 +3,23 @@ from project_management.timers import naptime
 from selenium_utilities.inputs import click_button
 from selenium_utilities.locators import locate_element_by_id
 
-from settings.county_variables.leopard import (disclaimer_active_class,
-                                               disclaimer_button_id,
-                                               disclaimer_id, open_script)
 from settings.general_functions import javascript_script_execution
 
 
 def handle_disclaimer(browser, abstract):
-    javascript_script_execution(browser, open_script)
-    disclaimer = locate_element_by_id(browser, disclaimer_id, "disclaimer", False)
-    if disclaimer.get_attribute('class') == disclaimer_active_class:
-        click_button(browser, locate_element_by_id, disclaimer_button_id, "disclaimer button")
+    javascript_script_execution(
+        browser,
+        abstract.county.other["Open Script"]
+    )
+    disclaimer = locate_element_by_id(browser,
+                                      abstract.county.ids["Disclaimer"],
+                                      "disclaimer"
+                                      )
+    if disclaimer.get_attribute('class') == abstract.county.classes["Disclaimer Active"]:
+        click_button(
+            browser,
+            locate_element_by_id,
+            abstract.county.buttons["Disclaimer"],
+            "disclaimer button"
+        )
         naptime()
