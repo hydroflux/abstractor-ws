a86cc388ba5448d97be35c79753c82c3675c2d80
Jack Hubert <jphubert93@gmail.com>
2022-02-03 10:43:18 -0700

    updated the 'update_county_attributes' function

--- engines/jaguar/transform.py
+++ engines/jaguar/transform.py
@@ -19,15 +19,56 @@ def convert_document_numbers(abstract):
 
 
 def update_county_attributes(abstract):
+    # abstract.county.credentials = credentials  # Login
+    abstract.county.urls = {
+        # Login
+        # Search
+    }
+    abstract.county.titles = {
+        # Login
+    }
+    abstract.county.buttons = {
+        # Login
+        # Disclaimer
+        # Search
+        # Download
+    }
+    abstract.county.classes = {
+        # Login
+        # Open Document
+        # Record
+        # Download
+        # Error Handling
+    }
+    abstract.county.ids = {
+        # Record
+    }
+    abstract.county.inputs = {  # Consider changing to 'search_inputs'
+        # Search
+    }
+    abstract.county.messages = {
+        # Login
+        # Open Document
+        # Record
+        # Error Handling
+    }
+    abstract.county.tags = {
+        # Open Document
+        # Record
+    }
+    abstract.county.other = {
+        # Disclaimer
+        # Record
+    }
 
-    for document in abstract.document_list:
-        document.input_attributes = {
-            "Reception Number": reception_number_input_id,
-            "Name": name_input_id
-        }
-        document.button_attributes = {
-            "Submit Search": search_button_name
-        }
+    # for document in abstract.document_list:
+    #     document.input_attributes = {
+    #         "Reception Number": reception_number_input_id,
+    #         "Name": name_input_id
+    #     }
+    #     document.button_attributes = {
+    #         "Submit Search": search_button_name
+    #     }
 
 
 def transform(abstract):
