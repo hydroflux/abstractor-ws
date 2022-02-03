f8418c8215773b5db8454c5de78102731fff7553
Jack Hubert <jphubert93@gmail.com>
2022-02-03 11:58:13 -0700

    dropped the jaguar settings login imports in favor of using County attributes

--- engines/jaguar/login.py
+++ engines/jaguar/login.py
@@ -2,9 +2,7 @@ from selenium_utilities.inputs import click_button
 from selenium_utilities.locators import locate_element_by_name
 from selenium_utilities.open import open_url
 
-from settings.county_variables.jaguar import home_page_title, home_page_url, login_button_name
-
 
 def login(browser, abstract):
-    open_url(browser, home_page_url, home_page_title, "county site")
-    click_button(browser, locate_element_by_name, login_button_name, "login button")
+    open_url(browser, abstract.county.urls["Home Page"], abstract.county.titles["Home Page"], "county site")
+    click_button(browser, locate_element_by_name, abstract.county.buttons["Login"], "login button")
