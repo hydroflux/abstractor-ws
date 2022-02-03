738a4d0288704d455878a8ccfb186a7c10817580
Jack Hubert <jphubert93@gmail.com>
2022-02-03 10:39:21 -0700

    changed the format of the 'update_element_attributes' function in jaguar 'transform' to match the eagle 'transform' script; changed the function name to 'update_county_attributes'

--- engines/jaguar/transform.py
+++ engines/jaguar/transform.py
@@ -11,14 +11,51 @@ def update_document_attributes(abstract):
 
 # Similar to the 'leopard' convert_document_numbers function
 def convert_document_numbers(abstract):
-    for document in abstract.document_list:
-        if document.type == "document_number" and document.value.find("-") != -1:
-            document_number, year = document.value.split("-")
-            document.year = int(year)
-            document.value = f'{year}{document_number.zfill(6)}'
+    # abstract.county.credentials = credentials  # Login
+    abstract.county.urls = {
+        # Login
+        # Search
+    }
+    abstract.county.titles = {
+        # Login
+    }
+    abstract.county.buttons = {
+        # Login
+        # Disclaimer
+        # Search
+        # Download
+    }
+    abstract.county.classes = {
+        # Login
+        # Open Document
+        # Record
+        # Download
+        # Error Handling
+    }
+    abstract.county.ids = {
+        # Record
+    }
+    abstract.county.inputs = {  # Consider changing to 'search_inputs'
+        # Search
+    }
+    abstract.county.messages = {
+        # Login
+        # Open Document
+        # Record
+        # Error Handling
+    }
+    abstract.county.tags = {
+        # Open Document
+        # Record
+    }
+    abstract.county.other = {
+        # Disclaimer
+        # Record
+    }
 
 
-def update_element_attributes(abstract):
+def update_county_attributes(abstract):
+
     for document in abstract.document_list:
         document.input_attributes = {
             "Reception Number": reception_number_input_id,
@@ -32,4 +69,4 @@ def update_element_attributes(abstract):
 def transform(abstract):
     update_document_attributes(abstract)
     convert_document_numbers(abstract)
-    update_element_attributes(abstract)
+    update_county_attributes(abstract)
