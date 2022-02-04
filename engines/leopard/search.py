fa91c38eea68d0db22c0799aaaae40216b2a63ec
Jack Hubert <jphubert93@gmail.com>
2022-02-04 14:50:54 -0700

    dropped all variable imports in the leopard 'search' script and updated variable calls with their appropriate Abstract County attributes

--- engines/leopard/search.py
+++ engines/leopard/search.py
@@ -6,16 +6,6 @@ from selenium_utilities.element_interaction import get_parent_element, is_active
 from selenium_utilities.inputs import clear_input, click_button, enter_input_value
 from selenium_utilities.locators import locate_element_by_id
 
-from settings.county_variables.leopard import (book_and_page_search_button_id,
-                                               book_and_page_search_tab_id,
-                                               book_search_field_id,
-                                               document_search_button_id,
-                                               document_search_field_id,
-                                               document_search_tab_id,
-                                               page_search_field_id,
-                                               search_navigation_id,
-                                               search_script, search_title)
-
 from settings.general_functions import javascript_script_execution
 
 # Use the following print statement to identify the best way to manage imports for Django vs the script folder
@@ -23,30 +13,33 @@ print("search", __name__)
 
 
 def access_search_navigation_tab(browser, abstract, document):
-    search_navigation_tab = locate_element_by_id(browser, search_navigation_id, "search navigation",
-                                                 True, document)
+    search_navigation_tab = locate_element_by_id(browser, abstract.county.ids["Search Navigation"],
+                                                 "search navigation", True, document)
     while search_navigation_tab is None:
-        search_navigation_tab = locate_element_by_id(browser, search_navigation_id, "search navigation",
-                                                     True, document)
+        search_navigation_tab = locate_element_by_id(browser, abstract.county.ids["Search Navigation"],
+                                                     "search navigation", True, document)
     return search_navigation_tab
 
 
 def open_search(browser, abstract, document):
-    javascript_script_execution(browser, search_script)
+    javascript_script_execution(browser, abstract.county.other["Search Script"])
     navigation_tab = access_search_navigation_tab(browser, abstract, document)
     while not is_active_class(navigation_tab):
         print("Navigation tab not active, attempting to connect again.")
         naptime()  # Allows time for navigation to load
         navigation_tab = access_search_navigation_tab(browser, abstract, document)
-    assert search_title
+    assert abstract.county.titles["Search"]
 
 
 def clear_search(browser, abstract, document):
     if document.type == "document_number":
-        clear_input(browser, locate_element_by_id, document_search_field_id, "document search field", document)
+        clear_input(browser, locate_element_by_id, abstract.county.inputs["Reception Number"],
+                    "document search field", document)
     elif document.type == "book_and_page_number":
-        clear_input(browser, locate_element_by_id, book_search_field_id, "book search field", document)
-        clear_input(browser, locate_element_by_id, page_search_field_id, "page search field", document)
+        clear_input(browser, locate_element_by_id, abstract.county.inputs["Book"],
+                    "book search field", document)
+        clear_input(browser, locate_element_by_id, abstract.county.inputs["Page"],
+                    "page search field", document)
 
 
 def access_search_type_tab(browser, document, attribute, type):
@@ -65,31 +58,33 @@ def open_tab(browser, document, attribute, type):
 
 
 def execute_document_number_search(browser, abstract, document):
-    open_tab(browser, document, document_search_tab_id, "document search tab")
+    open_tab(browser, document, abstract.county.ids["Document Search Tab"], "document search tab")
     # dropped a 'scroll_into_view' before entering inputs => update the 'enter_input_value' function accordingly
     enter_input_value(
         browser,
         locate_element_by_id,
-        document_search_field_id,
+        abstract.county.inputs["Reception Number"],
         "document search field",
         document.document_value()
     )
     click_button(  # Execute Search
         browser,
         locate_element_by_id,
-        document_search_button_id,
+        abstract.county.buttons["Document Search"],
         "document search button",
         document
     )
 
 
 def execute_book_and_page_search(browser, abstract, document):
-    open_tab(browser, document, book_and_page_search_tab_id, "book and page search tab")
+    open_tab(browser, document, abstract.county.ids["Book And Page Search Tab"], "book and page search tab")
     book, page = document.document_value()
     # dropped a 'scroll_into_view' before entering inputs => update the 'enter_input_value' function accordingly
-    enter_input_value(browser, locate_element_by_id, book_search_field_id, "book search field", book, document)
-    enter_input_value(browser, locate_element_by_id, page_search_field_id, "page search field", page, document)
-    click_button(browser, locate_element_by_id, book_and_page_search_button_id,
+    enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Book"],
+                      "book search field", book, document)
+    enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Page"],
+                      "page search field", page, document)
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Book And Page Search"],
                  "book and page search button", document)  # Execute Search
 
 
