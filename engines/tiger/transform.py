f8d020548bb68a7ddce563f25b8252d7b488d46a
Jack Hubert <jphubert93@gmail.com>
2022-02-05 09:40:44 -0700

    created an 'update_county_attributes' function in the tiger 'transform' script modeled after the 'leopard' function of the same name

--- engines/tiger/transform.py
+++ engines/tiger/transform.py
@@ -14,7 +14,74 @@ def convert_document_numbers(abstract):
     pass
 
 
+def update_county_attributes(abstract):
+    abstract.county.urls = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.titles = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.buttons = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.classes = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.ids = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.inputs = {  # Consider changing to 'search_inputs'
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.messages = {  # Consider changing to 'search_inputs'
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.tags = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+    abstract.county.other = {
+        # LOGIN
+        # SEARCH
+        # OPEN DOCUMENT
+        # RECORD
+        # DOWNLOAD
+    }
+
+
 # Identical to the 'leopard' transform_document_list function
 def transform_document_list(abstract):
     update_document_attributes(abstract)
     convert_document_numbers(abstract)
+    update_county_attributes(abstract)
