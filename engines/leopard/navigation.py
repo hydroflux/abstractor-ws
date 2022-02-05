93b3845227d18dff5f71097f15895877c7313c54
Jack Hubert <jphubert93@gmail.com>
2022-02-05 09:32:55 -0700

    added abstract parameter into the 'navigation' functions and updated variable imports to use County attributes instead

--- engines/leopard/navigation.py
+++ engines/leopard/navigation.py
@@ -1,17 +1,15 @@
 from selenium_utilities.inputs import click_button
 from selenium_utilities.locators import locate_element_by_id
 
-from settings.county_variables.leopard import (next_result_id,
-                                               previous_result_id)
 from settings.general_functions import scroll_to_top
 
 
-def previous_result(browser, document):
+def previous_result(browser, abstract, document):
     scroll_to_top(browser)
-    click_button(browser, locate_element_by_id, previous_result_id, "previous result", document)
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Previous Result"], "previous result", document)
 
 
-def next_result(browser, document):
+def next_result(browser, abstract, document):
     # next_result_button = get_next_result_button(browser, document)
     scroll_to_top(browser)  # should 'scroll_to_top' vs. 'center' be an option when clicking a button?
-    click_button(browser, locate_element_by_id, next_result_id, "next result", document)
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Next Result"], "next result", document)
