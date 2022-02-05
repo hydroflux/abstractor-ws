adf24ae4580259ddcab67810cbdda1f5ee07c042
Jack Hubert <jphubert93@gmail.com>
2022-02-05 10:42:03 -0700

    cleaned up the tiger 'transform' namespace by importing the entire settings script and updating their calls

--- engines/tiger/transform.py
+++ engines/tiger/transform.py
@@ -1,20 +1,4 @@
-from settings.county_variables.tiger import (book_page_abbreviation,
-                                             credentials, document_image_id,
-                                             document_information_id,
-                                             document_search_field_id,
-                                             download_button_id,
-                                             first_result_tag,
-                                             handle_disclaimer_button_id,
-                                             login_button_name, login_page,
-                                             login_title, result_cell_tag,
-                                             result_count_button_id,
-                                             result_count_id, results_body_tag,
-                                             results_table_id, row_titles,
-                                             search_button_id,
-                                             search_navigation_id,
-                                             search_script, search_tab_id,
-                                             search_title, stock_download,
-                                             view_panel_id)
+import settings.county_variables.tiger as tiger
 
 
 # Identical to the 'leopard' update_document_attributes function
@@ -22,7 +6,7 @@ from settings.county_variables.tiger import (book_page_abbreviation,
 def update_document_attributes(abstract):
     for document in abstract.document_list:
         document.county = abstract.county
-        document.download_value = stock_download
+        document.download_value = tiger.stock_download
 
 
 def convert_document_numbers(abstract):
@@ -31,66 +15,66 @@ def convert_document_numbers(abstract):
 
 
 def update_county_attributes(abstract):
-    abstract.county.credentials = credentials
+    abstract.county.credentials = tiger.credentials
     abstract.county.urls = {
         # LOGIN
-        "Login": login_page
+        "Login": tiger.login_page
     }
     abstract.county.titles = {
         # LOGIN
-        "Login": login_title,
+        "Login": tiger.login_title,
         # SEARCH
-        "Search": search_title,
+        "Search": tiger.search_title,
         # RECORD
-        "Row Titles": row_titles
+        "Row Titles": tiger.row_titles
     }
     abstract.county.buttons = {
         # LOGIN
-        "Login": login_button_name,
-        "Disclaimer": handle_disclaimer_button_id,
+        "Login": tiger.login_button_name,
+        "Disclaimer": tiger.handle_disclaimer_button_id,
         # SEARCH
-        "Search": search_button_id,
+        "Search": tiger.search_button_id,
         # OPEN DOCUMENT
-        "Result Count": result_count_button_id,
+        "Result Count": tiger.result_count_button_id,
         # DOWNLOAD
-        "Download Submenu": view_panel_id,
-        "Download": download_button_id
+        "Download Submenu": tiger.view_panel_id,
+        "Download": tiger.download_button_id
     }
     abstract.county.classes = {
     }
     abstract.county.ids = {
         # SEARCH
-        "Search Navigation": search_navigation_id,
-        "Search Tab": search_tab_id,
+        "Search Navigation": tiger.search_navigation_id,
+        "Search Tab": tiger.search_tab_id,
         # OPEN DOCUMENT
-        "Result County": result_count_id,
-        "Results Table": results_table_id,
+        "Result County": tiger.result_count_id,
+        "Results Table": tiger.results_table_id,
         # RECORD
-        "Document Image": document_image_id,
-        "Document Information": document_information_id,
+        "Document Image": tiger.document_image_id,
+        "Document Information": tiger.document_information_id,
     }
     abstract.county.inputs = {  # Consider changing to 'search_inputs'
         # SEARCH
-        "Reception Number": document_search_field_id
+        "Reception Number": tiger.document_search_field_id
     }
     abstract.county.messages = {  # Consider changing to 'search_inputs'
     }
     abstract.county.scripts = {
         # SEARCH
-        "Search": search_script
+        "Search": tiger.search_script
     }
     abstract.county.tags = {
         # OPEN / RECORD DOCUMENT
-        "Body": results_body_tag,
-        "Rows": first_result_tag,
-        "Data": result_cell_tag,
+        "Body": tiger.results_body_tag,
+        "Rows": tiger.first_result_tag,
+        "Data": tiger.result_cell_tag,
         # "Body": document_table_tag,
         # "Rows": table_row_tag,
         # "Data": row_data_tag
     }
     abstract.county.other = {
         # RECORD
-        "Abbreviation": book_page_abbreviation
+        "Abbreviation": tiger.book_page_abbreviation
     }
 
 
