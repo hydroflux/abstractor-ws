0e37da8a3bd7af9e1106bf1e475e4b7d8abc82a5
Jack Hubert <jphubert93@gmail.com>
2022-02-05 10:35:39 -0700

    updated all of the 'record' functions that need to have the current Abstract instance passed in to use the County attributes set in the 'transform' script

--- engines/tiger/record.py
+++ engines/tiger/record.py
@@ -9,7 +9,7 @@ from settings.general_functions import get_element_text
 print("record", __name__)
 
 
-def access_document_information(browser, document):
+def access_document_information(browser, abstract, document):
     locate_element_by_id(browser, abstract.county.ids["Document Image"],  # Document Image Loaded
                          "document image", False, document)
     document_information = locate_element_by_id(browser, abstract.county.ids["Document Information"],  # Document Information Loaded
@@ -17,23 +17,23 @@ def access_document_information(browser, document):
     return document_information
 
 
-def access_document_table_data(browser, document):
-    document_information = access_document_information(browser, document)
+def access_document_table_data(browser, abstract, document):
+    document_information = access_document_information(browser, abstract, document)
     table_data = locate_element_by_tag_name(document_information, abstract.county.tags["Data"], "table data", False, document)
     return table_data
 
 
-def get_table_rows(document_table):
+def get_table_rows(abstract, document_table):
     return document_table.find_elements_by_tag_name(abstract.county.tags["Rows"])
 
 
-def get_row_data(row):
+def get_row_data(abstract, row):
     row_data = row.find_elements_by_tag_name(abstract.county.tags["Rows"])
     return get_element_text(row_data[0]), get_element_text(row_data[1])
 
 
-def get_row_value(row, title):
-    row_title, row_content = get_row_data(row)
+def get_row_value(abstract, row, title):
+    row_title, row_content = get_row_data(abstract, row)
     if row_title == title:
         return row_content
     else:
@@ -41,13 +41,13 @@ def get_row_value(row, title):
 
 
 def record_reception_number(abstract, row, document):
-    reception_number = get_row_value(row, abstract.county.titles["Row Titles"]["reception_number"])
+    reception_number = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["reception_number"])
     document.reception_number = reception_number
     record_value(abstract, 'reception number', reception_number)
 
 
 def record_book_and_page(abstract, row):
-    book_page_value = get_row_value(row, abstract.county.titles["Row Titles"]["book_and_page"])
+    book_page_value = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["book_and_page"])
     if book_page_value.startswith(abstract.county.other["Abbreviation"]):
         book, page = book_page_value[len(abstract.county.other["Abbreviation"]):].split("/")
         book = book.strip()
@@ -66,32 +66,32 @@ def record_book_and_page(abstract, row):
 
 
 def record_recording_date(abstract, row):
-    recording_date = get_row_value(row, abstract.county.titles["Row Titles"]["recording_date"])
+    recording_date = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["recording_date"])
     record_value(abstract, 'recording date', recording_date[:10])
 
 
 def record_document_type(abstract, row):
-    document_type = get_row_value(row, abstract.county.titles["Row Titles"]["document_type"])
+    document_type = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["document_type"])
     record_value(abstract, 'document_type', document_type.title())
 
 
 def record_grantor(abstract, row):
-    grantor = get_row_value(row, abstract.county.titles["Row Titles"]["grantor"])
+    grantor = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["grantor"])
     record_value(abstract, 'grantor', grantor.title())
 
 
 def record_grantee(abstract, row):
-    grantee = get_row_value(row, abstract.county.titles["Row Titles"]["grantee"])
+    grantee = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["grantee"])
     record_value(abstract, 'grantee', grantee.title())
 
 
 def record_related_documents(abstract, row):
-    related_documents = get_row_value(row, abstract.county.titles["Row Titles"]["related_documents"])
+    related_documents = get_row_value(abstract, row, abstract.county.titles["Row Titles"]["related_documents"])
     record_value(abstract, 'related documents', related_documents)
 
 
 def record_legal(abstract, row_1):
-    legal = get_row_value(row_1, abstract.county.titles["Row Titles"]["legal"])
+    legal = get_row_value(abstract, row_1, abstract.county.titles["Row Titles"]["legal"])
     # additional_legal = get_row_value(row_2, row_titles["additional_legal"])
     # if legal != additional_legal:
     #     abstract.dataframe["Legal"].append(f'{legal}\n{additional_legal}')
@@ -113,6 +113,6 @@ def aggregate_document_information(abstract, rows, document):
 
 # Write a function to check additional information for rows 4, 7
 def record(browser, abstract, document):
-    document_table = access_document_table_data(browser, document)
-    rows = get_table_rows(document_table)
+    document_table = access_document_table_data(browser, abstract, document)
+    rows = get_table_rows(abstract, document_table)
     aggregate_document_information(abstract, rows, document)
