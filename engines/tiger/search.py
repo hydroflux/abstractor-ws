18dd0ac8a8969041caebad5afe4dfe62faa07507
Jack Hubert <jphubert93@gmail.com>
2022-02-05 10:14:38 -0700

    integrated County attributes into the 'search' functions of the tiger 'search' script and dropped the variable imports

--- engines/tiger/search.py
+++ engines/tiger/search.py
@@ -6,30 +6,25 @@ from selenium_utilities.inputs import (clear_input, click_button,
                                        enter_input_value)
 from selenium_utilities.locators import locate_element_by_id
 
-from settings.county_variables.tiger import (instrument_search_id,
-                                             search_button_id,
-                                             search_navigation_id,
-                                             search_script, search_tab_id,
-                                             search_title)
 from settings.general_functions import javascript_script_execution
 
 
 def open_search(browser, abstract):
     # Messy => figure out a better way to do this
-    javascript_script_execution(search_script)
+    javascript_script_execution(abstract.county.scripts["Search"])
     naptime()
     # This will probably not work great when called during the 'login' process
     ######
-    search_navigation = locate_element_by_id(browser, search_navigation_id, "search navigation", True)
+    search_navigation = locate_element_by_id(browser, abstract.county.ids["Search Navigation"], "search navigation", True)
     if is_active_class(search_navigation):
         return
     else:
-        browser.execute_script(search_script)
-        assert search_title
+        browser.execute_script(abstract.county.scripts["Search"])
+        assert abstract.county.titles["Search"]
 
 
 def open_search_tab(browser, abstract):
-    search_tab = locate_element_by_id(browser, search_tab_id, "search tab", True)
+    search_tab = locate_element_by_id(browser, abstract.county.ids["Search Tab"], "search tab", True)
     center_element(browser, search_tab)
     if is_active_class(get_parent_element(search_tab)):
         return
@@ -38,13 +33,13 @@ def open_search_tab(browser, abstract):
 
 
 def clear_search(browser, abstract, document):
-    clear_input(browser, locate_element_by_id, instrument_search_id, "reception number input", document)
+    clear_input(browser, locate_element_by_id, abstract.county.inputs["Reception Number"], "reception number input", document)
 
 
 def handle_document_value_numbers(browser, abstract, document):
     value = document.value()
     if document.type == "document_number":
-        enter_input_value(browser, locate_element_by_id, instrument_search_id,
+        enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Reception Number"],
                           "reception number input", value, document)
     else:
         print(f'Unable to search document type "{document.type}", '
@@ -55,7 +50,7 @@ def handle_document_value_numbers(browser, abstract, document):
 
 def execute_search(browser, abstract, document):
     handle_document_value_numbers(browser, abstract, document)
-    click_button(browser, locate_element_by_id, search_button_id,
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Search"],
                  "execute search button", document)
 
 
