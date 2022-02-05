3c80fb370243d1204beada741e3214bd9dd9ad66
Jack Hubert <jphubert93@gmail.com>
2022-02-05 10:32:55 -0700

    dropped variable imports from the tiger 'record' script and replaced with attributes set in the 'transform' script

--- engines/tiger/record.py
+++ engines/tiger/record.py
@@ -3,11 +3,6 @@ from selenium_utilities.locators import locate_element_by_id, locate_element_by_
 from serializers.recorder import record_comments, record_value
 
 from settings.county_variables.general import not_applicable
-from settings.county_variables.tiger import (book_page_abbreviation,
-                                             document_image_id,
-                                             document_information_id,
-                                             document_table_tag, row_data_tag,
-                                             row_titles, table_row_tag)
 from settings.general_functions import get_element_text
 
 # Use the following print statement to identify the best way to manage imports for Django vs the script folder
@@ -15,25 +10,25 @@ print("record", __name__)
 
 
 def access_document_information(browser, document):
-    locate_element_by_id(browser, document_image_id,  # Document Image Loaded
+    locate_element_by_id(browser, abstract.county.ids["Document Image"],  # Document Image Loaded
                          "document image", False, document)
-    document_information = locate_element_by_id(browser, document_information_id,  # Document Information Loaded
+    document_information = locate_element_by_id(browser, abstract.county.ids["Document Information"],  # Document Information Loaded
                                                 "document information", False, document)
     return document_information
 
 
 def access_document_table_data(browser, document):
     document_information = access_document_information(browser, document)
-    table_data = locate_element_by_tag_name(document_information, document_table_tag, "table data", False, document)
+    table_data = locate_element_by_tag_name(document_information, abstract.county.tags["Data"], "table data", False, document)
     return table_data
 
 
 def get_table_rows(document_table):
-    return document_table.find_elements_by_tag_name(table_row_tag)
+    return document_table.find_elements_by_tag_name(abstract.county.tags["Rows"])
 
 
 def get_row_data(row):
-    row_data = row.find_elements_by_tag_name(row_data_tag)
+    row_data = row.find_elements_by_tag_name(abstract.county.tags["Rows"])
     return get_element_text(row_data[0]), get_element_text(row_data[1])
 
 
@@ -46,15 +41,15 @@ def get_row_value(row, title):
 
 
 def record_reception_number(abstract, row, document):
-    reception_number = get_row_value(row, row_titles["reception_number"])
+    reception_number = get_row_value(row, abstract.county.titles["Row Titles"]["reception_number"])
     document.reception_number = reception_number
     record_value(abstract, 'reception number', reception_number)
 
 
 def record_book_and_page(abstract, row):
-    book_page_value = get_row_value(row, row_titles["book_and_page"])
-    if book_page_value.startswith(book_page_abbreviation):
-        book, page = book_page_value[len(book_page_abbreviation):].split("/")
+    book_page_value = get_row_value(row, abstract.county.titles["Row Titles"]["book_and_page"])
+    if book_page_value.startswith(abstract.county.other["Abbreviation"]):
+        book, page = book_page_value[len(abstract.county.other["Abbreviation"]):].split("/")
         book = book.strip()
         page = page.strip()
         if book == '0' and page == '0':
@@ -71,32 +66,32 @@ def record_book_and_page(abstract, row):
 
 
 def record_recording_date(abstract, row):
-    recording_date = get_row_value(row, row_titles["recording_date"])
+    recording_date = get_row_value(row, abstract.county.titles["Row Titles"]["recording_date"])
     record_value(abstract, 'recording date', recording_date[:10])
 
 
 def record_document_type(abstract, row):
-    document_type = get_row_value(row, row_titles["document_type"])
+    document_type = get_row_value(row, abstract.county.titles["Row Titles"]["document_type"])
     record_value(abstract, 'document_type', document_type.title())
 
 
 def record_grantor(abstract, row):
-    grantor = get_row_value(row, row_titles["grantor"])
+    grantor = get_row_value(row, abstract.county.titles["Row Titles"]["grantor"])
     record_value(abstract, 'grantor', grantor.title())
 
 
 def record_grantee(abstract, row):
-    grantee = get_row_value(row, row_titles["grantee"])
+    grantee = get_row_value(row, abstract.county.titles["Row Titles"]["grantee"])
     record_value(abstract, 'grantee', grantee.title())
 
 
 def record_related_documents(abstract, row):
-    related_documents = get_row_value(row, row_titles["related_documents"])
+    related_documents = get_row_value(row, abstract.county.titles["Row Titles"]["related_documents"])
     record_value(abstract, 'related documents', related_documents)
 
 
 def record_legal(abstract, row_1):
-    legal = get_row_value(row_1, row_titles["legal"])
+    legal = get_row_value(row_1, abstract.county.titles["Row Titles"]["legal"])
     # additional_legal = get_row_value(row_2, row_titles["additional_legal"])
     # if legal != additional_legal:
     #     abstract.dataframe["Legal"].append(f'{legal}\n{additional_legal}')
