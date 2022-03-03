8e39c14ed86d94ca64f951f19cca4a0dded718a9
Jack Hubert <jphubert93@gmail.com>
2022-03-02 20:42:22 -0700

    built out a function scaffold for the mountain lion 'search' script

--- engines/mountain_lion/search.py
+++ engines/mountain_lion/search.py
@@ -11,11 +11,41 @@ def open_search_page(browser, abstract, document):
                  abstract.county.buttons['Open Search'], "open search", document)
 
 
-def process_document_search(browser, document):
+# Use leopard as a guide when adding book & page routes
+def open_search_type_tab(browser, abstract, document):
     pass
+
+
+# Use leopard as a guide when adding book & page routes
+def clear_search(browser, abstract, document):
+    pass
+
+
+# Use leopard as a guide when adding book & page routes
+def enter_input(browser, abstract, document):
+    pass
+
+
+# Use leopard as a guide when adding book & page routes
+def execute_search(browser, abstract, document):
+    pass
+
+
+# Use leopard as a guide when adding book & page routes
+def execute_document_number_search(browser, abstract, document):
+    pass
+
+
+def handle_document_search(browser, abstract, document):
+    if document.type == "document_number":
+        execute_document_number_search(browser, abstract, document)
+    else:
+        print(f'Unable to search "{document.type}" document type, new search path needs to be developed.')
+        print("Please press enter after reviewing the search parameters...")
+        input()
 
 
 def search(browser, abstract, document):
     open_search_page(browser, abstract, document)
     if page_is_loaded(browser, abstract, abstract.county.messages['Search Header']):
-        process_document_search(browser, document)
+        handle_document_search(browser, abstract, document)
