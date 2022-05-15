de6e0bfed5706a81ff0c90dadb779f897e3d080a
Jack Hubert <jphubert93@gmail.com>
2022-05-14 18:46:30 -0600

    added 'execute_name_search' function to the eagle 'execute' file

--- engines/eagle/execute.py
+++ engines/eagle/execute.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+from engines.eagle.name_search import name_search
+from project_management.export import export_document
 from serializers.executor import close_program, search_documents_from_list
 
 from settings.driver import create_webdriver
@@ -30,6 +32,15 @@ def execute_program(abstract):
         execute_download,
         next_result
     )
+    close_program(browser, abstract)
+
+
+def execute_name_search(abstract):
+    browser = create_webdriver(abstract)
+    login(browser, abstract)
+    name_search(browser, abstract)
+    project = export_document(abstract)
+    project.bundle_project(abstract)
     close_program(browser, abstract)
 
 
