8f48cdcb5318dc00cbe0f5055874bd7ab58441c2
Jack Hubert <jphubert93@gmail.com>
2022-05-17 17:59:33 -0600

    updated open_document script during testing; moved the 'center_element' function to handle a different page element

--- engines/tiger/open_document.py
+++ engines/tiger/open_document.py
@@ -1,3 +1,4 @@
+from project_management.timers import naptime
 from selenium_utilities.element_interaction import (center_element,
                                                     get_element_text)
 from selenium_utilities.inputs import click_button
@@ -9,16 +10,20 @@ from selenium_utilities.locators import (locate_element_by_id,
 def count_results(browser, abstract, document):
     click_button(browser, locate_element_by_id, abstract.county.buttons["Result Count"],
                  "result count button", document)  # Open Result Count
+    naptime()
     result_count = locate_element_by_id(browser, abstract.county.ids["Result Count"],
                                         "result count", document=document)  # Get Result Count
-    return result_count.text.split(' ')[-1]
+    # This doesn't work the way it's supposed to
+    # Need to perform a match against the document number in order to find the correct count
+    # Check leopard for options
+    if int(result_count.text.split(' ')[-1]) > 1:
+        document.number_results = 1
 
 
 def access_results_table(browser, abstract, document):
     results = locate_element_by_id(browser, abstract.county.ids["Results Table"],
                                    "results section", document=document)
-    center_element(browser, results)
-    results_table = locate_element_by_tag_name(browser, abstract.county.tags["Body"],
+    results_table = locate_element_by_tag_name(results, abstract.county.tags["Body"],
                                                "results table", document=document)
     return results_table
 
@@ -29,16 +34,17 @@ def access_first_row(abstract, document, results_table):
     return all_results[0]
 
 
-def check_result(abstract, document, results_table):
+def check_result(browser, abstract, document, results_table):
     first_result = access_first_row(abstract, document, results_table)
+    center_element(browser, first_result)
     first_result_cells = first_result.find_elements_by_tag_name(abstract.county.tags["Data"])
-    if document.value() in map(get_element_text, first_result_cells):
+    if document.value in map(get_element_text, first_result_cells):
         return True
 
 
 def open_document(browser, abstract, document):
     count_results(browser, abstract, document)
     results_table = access_results_table(browser, abstract, document)
-    if check_result(abstract, document, results_table):
+    if check_result(browser, abstract, document, results_table):
         access_first_row(abstract, document, results_table).click()
         return True
