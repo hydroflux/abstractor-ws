238d58a48e93a5ac0713e6147be26fc61b1ad1cd
Jack Hubert <jphubert93@gmail.com>
2022-05-16 20:27:47 -0600

    created a basic error_handling script for rabbit

---
+++ engines/rabbit/error_handling.py
@@ -0,0 +1,4 @@
+def check_for_browser_error(browser):
+    print('Browser has hit an unexpected error, please review and create a logical path to correct the issue.')
+    print('Press enter to continue...')
+    input()
