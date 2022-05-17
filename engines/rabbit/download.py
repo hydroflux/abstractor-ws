31d488d645e13ed5e74a029d75a2b60331a83b40
Jack Hubert <jphubert93@gmail.com>
2022-05-16 21:14:28 -0600

    created a 'download' script for rabbit

---
+++ engines/rabbit/download.py
@@ -0,0 +1,9 @@
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import locate_element_by_id
+
+
+def execute_download(browser, abstract, document):
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Download Submenu"],  # Open Download Submenu
+                 "download submenu button", document)
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Download"],  # Execute Download
+                 "execute download button", document)
