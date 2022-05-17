a97a86f212bc713c346b2f18dc7f6c9f41970895
Jack Hubert <jphubert93@gmail.com>
2022-05-16 20:29:25 -0600

    mapped out a simple execution script for rabbit and imported functions already created from rabbit scripts

---
+++ engines/rabbit/execute.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from serializers.executor import close_program, search_documents_from_list
+
+from settings.driver import create_webdriver
+
+# from engines.rabbit.download import execute_download
+from engines.rabbit.login import login
+# from engines.rabbit.logout import logout
+# from engines.rabbit.navigation import next_result
+# from engines.rabbit.open_document import open_document
+# from engines.rabbit.record import record
+from engines.rabbit.search import search
+from engines.rabbit.transform import transform
+
+
+def execute_program(abstract):
+    browser = create_webdriver(abstract)
+    transform(abstract)
+    login(browser, abstract)
+    search_documents_from_list(
+        browser,
+        abstract,
+        search,
+        # open_document,
+        # record,
+        # execute_download,
+        # next_result
+    )
+    close_program(browser, abstract)
