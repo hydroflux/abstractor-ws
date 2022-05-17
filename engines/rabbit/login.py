c12570a96900d1357f050642488fbb52a2169a7a
Jack Hubert <jphubert93@gmail.com>
2022-05-16 20:28:38 -0600

    created a disclaimer script for rabbit and integrated into the login function / script

--- engines/rabbit/login.py
+++ engines/rabbit/login.py
@@ -1,5 +1,7 @@
 from selenium_utilities.open import open_url
+from engines.rabbit.disclaimer import handle_disclaimer
 
 
 def login(browser, abstract):
     open_url(browser, abstract.county.urls["Home"], abstract.county.titles["Home"], "county site")
+    handle_disclaimer(browser, abstract)
