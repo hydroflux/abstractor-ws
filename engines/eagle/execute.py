3cd3f7b6ec87071f67eb708d83ece4bc87cd926a
Jack Hubert <jphubert93@gmail.com>
2022-05-21 14:42:18 -0600

    dropped unused comments from eagle execute

--- engines/eagle/execute.py
+++ engines/eagle/execute.py
@@ -12,11 +12,6 @@ from engines.eagle.open_document import open_document
 from engines.eagle.record import record
 from engines.eagle.search import search
 
-# from engines.eagle.transform import transform_document_list
-
-# Use the following print statement to identify the best way to manage imports for Django vs the script folder
-print("execute", __name__)
-
 
 # Identical to 'leopard', 'jaguar', 'rattlesnake', & 'tiger' execute_program
 def execute_program(abstract):
