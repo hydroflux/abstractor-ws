3cf644794cc91118ee4ac6345f6dc857ff47b682
Jack Hubert <jphubert93@gmail.com>
2022-05-21 15:55:16 -0600

    started working through name search script for rabbit

--- engines/rabbit/name_search.py
+++ engines/rabbit/name_search.py
@@ -1,2 +1,7 @@
+from classes.Document import Document
+from engines.rabbit.search import search
+
+
 def name_search(browser, abstract):
-    return
+    document = Document("name", abstract.search_name)
+    search(browser, abstract, document)
