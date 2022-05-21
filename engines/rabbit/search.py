7610dd82966799f6bc52f43aba1ddfbc4f2f5c64
Jack Hubert <jphubert93@gmail.com>
2022-05-21 15:56:00 -0600

    updated the rabbit search script to include name searches; remobved the 'access_search_navigation' function (unnecessary), but left in the original function for reference later on (commented out)

--- engines/rabbit/search.py
+++ engines/rabbit/search.py
@@ -8,23 +8,30 @@ from selenium_utilities.locators import locate_element_by_id
 from selenium_utilities.open import open_url
 
 
-def access_search_navigation_tab(browser, abstract, document):
-    search_navigation_tab = locate_element_by_id(browser, abstract.county.ids["Search Navigation"],
-                                                 "search navigation", True, document)
-    while search_navigation_tab is None:
-        search_navigation_tab = locate_element_by_id(browser, abstract.county.ids["Search Navigation"],
-                                                     "search navigation", True, document)
-    return search_navigation_tab
+# def access_search_navigation_tab(browser, abstract, document):
+#     search_navigation_tab = locate_element_by_id(browser, abstract.county.ids["Search Navigation"],
+#                                                  "search navigation", True, document)
+#     while search_navigation_tab is None:
+#         search_navigation_tab = locate_element_by_id(browser, abstract.county.ids["Search Navigation"],
+#                                                      "search navigation", True, document)
+#     search_navigation_tab.click()
+#     return search_navigation_tab
 
 
 def open_search(browser, abstract, document):
-    open_url(browser, abstract.county.urls['Search'],
-             abstract.county.titles['Search'], 'search page', document)
-    navigation_tab = access_search_navigation_tab(browser, abstract, document)
-    while not is_active_class(navigation_tab):
-        print("Navigation tab not active, attempting to connect again.")
-        naptime()  # Allows time for navigation to load
-        navigation_tab = access_search_navigation_tab(browser, abstract, document)
+    if document.type == "document_number":
+        open_url(browser, abstract.county.urls['Document Search'],
+                 abstract.county.titles['Document Search'], 'search page', document)
+    elif document.type == "name":
+        open_url(browser, abstract.county.urls['Name Search'],
+                 abstract.county.titles['Name Search'], 'document search page', document)
+    # navigation_tab = access_search_navigation_tab(browser, abstract, document)
+    # print("navigation tab", navigation_tab)
+    # print("")
+    # while not is_active_class(navigation_tab):
+    #     print("Navigation tab not active, attempting to connect again.")
+    #     naptime()  # Allows time for navigation to load
+    #     navigation_tab = access_search_navigation_tab(browser, abstract, document)
 
 
 def access_search_type_tab(browser, document, attribute, type):
@@ -45,6 +52,8 @@ def open_tab(browser, document, attribute, type):
 def open_search_type_tab(browser, abstract, document):
     if document.type == "document_number":
         open_tab(browser, document, abstract.county.ids["Document Search Tab"], "document search tab")
+    elif document.type == "name":
+        open_tab(browser, document, abstract.county.ids["Name Search Tab"], "name search tab")
     else:
         print(f'Abstractor path has not yet been developed to "open_search_type_tab" for document type "\
                {document.type}", please review...')
@@ -55,6 +64,13 @@ def clear_search(browser, abstract, document):
     if document.type == "document_number":
         clear_input(browser, locate_element_by_id, abstract.county.inputs["Reception Number"],
                     "document search field", document)
+    elif document.type == "name":
+        clear_input(browser, locate_element_by_id, abstract.county.inputs["Name"],
+                    "name search field", document)
+        clear_input(browser, locate_element_by_id, abstract.county.inputs["Start Date"],
+                    "name search start date field", document)
+        clear_input(browser, locate_element_by_id, abstract.county.inputs["End Date"],
+                    "name search end date field", document)
     else:
         print(f'Abstractor path has not yet been developed to "clear_search" for document type "\
                {document.type}", please review...')
@@ -65,6 +81,13 @@ def handle_document_value_numbers(browser, abstract, document):
     if document.type == "document_number":
         enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Reception Number"],
                           "document search field", document.document_value())
+    elif document.type == "name":
+        enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Name"],
+                          "name search field", document.document_value())
+        enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Start Date"],
+                          "name search start field", document.document_value())
+        enter_input_value(browser, locate_element_by_id, abstract.county.inputs["End Date"],
+                          "name search end date field", document.document_value())
     else:
         print(f'Abstractor path has not yet been developed to "handle_document_value_numbers" for document type "\
                {document.type}", please review...')
@@ -75,6 +98,9 @@ def execute_search(browser, abstract, document):
     if document.type == "document_number":
         click_button(browser, locate_element_by_id, abstract.county.buttons["Document Search"],
                      "document search button", document)
+    elif document.type == "name":
+        click_button(browser, locate_element_by_id, abstract.county.buttons["Name Search"],
+                    "name search button", document)
     else:
         print(f'Abstractor path has not yet been developed to "execute_search" for document type "\
                {document.type}", please review...')
