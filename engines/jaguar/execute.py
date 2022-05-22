88293c78d864e91b59636717863ec8527d404f28
Jack Hubert <jphubert93@gmail.com>
2022-05-22 15:25:33 -0600

    added the 'execute_download' function into the execute script

--- engines/jaguar/execute.py
+++ engines/jaguar/execute.py
@@ -5,7 +5,7 @@ from settings.driver import create_webdriver
 
 from engines.jaguar.download import execute_download
 from engines.jaguar.login import login
-from engines.jaguar.navigation import next_result
+# from engines.jaguar.navigation import next_result
 from engines.jaguar.open_document import open_document
 from engines.jaguar.record import record
 from engines.jaguar.search import search
@@ -20,7 +20,7 @@ def execute_program(abstract):
         search,
         open_document,
         record,
-        # execute_download,
+        execute_download,
         # next_result
     )
     close_program(browser, abstract)
