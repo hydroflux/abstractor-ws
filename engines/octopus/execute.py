fbe1190ab52a71e5d2fe166bcf4739241ed5609e
Jack Hubert <jphubert93@gmail.com>
2022-06-04 22:31:06 -0600

    imported the 'search_documents_from_list' for the octopus execute script and updated imports

--- engines/octopus/execute.py
+++ engines/octopus/execute.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-# from engines.octopus.open_document import get_results, open_result
-from project_management.export import export_document
-# from project_management.timers import naptime
-# from serializers.downloader import download_document
-
-from serializers.executor import close_program
-
-from settings.driver import create_webdriver
-
 from engines.octopus.collect import collect
-# from engines.octopus.download import execute_download
+from engines.octopus.download import execute_download
 from engines.octopus.login import login
 from engines.octopus.logout import logout
-# from engines.octopus.open_document import open_document
-# from engines.octopus.record import record
+from engines.octopus.open_document import open_document
+from engines.octopus.record import record
 from engines.octopus.search import search
+
+from serializers.executor import close_program, search_documents_from_list
+from settings.driver import create_webdriver
 
 
 def execute_legal_search(abstract):
@@ -22,18 +16,12 @@ def execute_legal_search(abstract):
     login(browser, abstract)
     search(browser, abstract)
     collect(browser, abstract)
-    # result_links = get_results(browser, abstract, document)
-    # for result in result_links[:5]:
-    #     # naptime()
-    #     open_result(browser, abstract, document, result)
-    #     record(browser, abstract, document)
-    #     download_document(browser, abstract, document, execute_download)
-    #     # naptime()
-    #     browser.back()
-    project = export_document(abstract)
-    project.bundle_project(abstract)
+    search_documents_from_list(
+        browser,
+        abstract,
+        search,
+        open_document,
+        record,
+        execute_download
+    )
     close_program(browser, abstract, logout)
-
-
-# Fix previous download issues
-# build a legal search loop to build the document list and then search from list
