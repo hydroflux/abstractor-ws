dd6efe020f1fcb669cc6706698a7c4be6eb768bc
Jack Hubert <jphubert93@gmail.com>
2022-06-04 22:46:41 -0600

    added a return statement to open document to integrate the function into the executor functions; added additional handling for cases where multiple results are returned at the same search point

--- engines/octopus/open_document.py
+++ engines/octopus/open_document.py
@@ -3,6 +3,7 @@ from settings.general_functions import javascript_script_execution
 
 
 def open_document(browser, abstract, document):
-    if not document.description_link:
+    if not document.description_link or document.result_number > 0:
         collect(browser, abstract, document)
     javascript_script_execution(browser, document.document_description)
+    return True
