ecc7e41a72ed35c6f5f3cf9a232d916d93c8667c
Jack Hubert <jphubert93@gmail.com>
2022-06-05 15:09:29 -0600

    created a 'demo' file to hold information for abstractor demos--previously commented out in the environment file

---
+++ project_management/demo.py
@@ -0,0 +1,17 @@
+# def execute_demo():
+#     county = get_county_data(get_demo_information())
+#     print(county)
+#     program_type = get_program_type()
+#     if program_type == "name_search":
+#         search_name = prepare_name_search()
+#         start_time = start_program_timer(county)
+#         execute_program_type(county, program_type, document_list=None, search_name=search_name)
+#     else:
+#         document_list = generate_document_list(target_directory, file_name, sheet_name)
+#         start_time = start_program_timer(county)
+#         execute_program_type(county, program_type, document_list)
+#     stop_program_timer(start_time)
+#     quit()
+
+
+# execute_demo()
