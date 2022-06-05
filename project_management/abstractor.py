ae7c51646bed400a66ac8bdc0a1b28502900656f
Jack Hubert <jphubert93@gmail.com>
2022-06-05 16:37:22 -0600

    changed the 'engine_switch' function to return the functions without their parameters listed; imported the webdriver so that it's created at the 'abstractor' level

--- project_management/abstractor.py
+++ project_management/abstractor.py
@@ -1,3 +1,5 @@
+from settings.driver import create_webdriver
+
 from engines.eagle.execute import execute_name_search as eagle_name_search
 from engines.eagle.execute import execute_program as execute_eagle
 from engines.jaguar.execute import execute_program as execute_jaguar
@@ -18,41 +20,51 @@ def engine_switch(abstract):
     if abstract.county.engine == 'eagle':
         abstract.headless = False
         if abstract.program in ["execute", "review", "download"]:
-            return execute_eagle(abstract)
+            return execute_eagle
+            # return execute_eagle(abstract)
         elif abstract.program == "name_search":
-            return eagle_name_search(abstract)
+            return eagle_name_search
+            # return eagle_name_search(abstract)
     elif abstract.county.engine == 'jaguar':
         if abstract.program in ["execute", "review", "download"]:
-            return execute_jaguar(abstract)
+            return execute_jaguar
+            # return execute_jaguar(abstract)
     elif abstract.county.engine == 'leopard':
         if abstract.program in ["execute", "review", "download"]:
             if abstract.program == "download":
                 abstract.headless = False
-            return execute_leopard(abstract)
+            return execute_leopard
+            # return execute_leopard(abstract)
     elif abstract.county.engine == 'octopus':
         if abstract.program in ["legal"]:
             if abstract.program == "download":
                 abstract.headless = False
-            return execute_octopus_legal(abstract)
+            return execute_octopus_legal
+            # return execute_octopus_legal(abstract)
     elif abstract.county.engine == 'tiger':
         if abstract.program in ["execute", "review", "download"]:
             if abstract.program == "download":
                 abstract.headless = False
-            return execute_tiger(abstract)
+            return execute_tiger
+            # return execute_tiger(abstract)
     elif abstract.county.engine == 'rabbit':
         if abstract.program in ["execute", "review", "download"]:
-            return execute_rabbit(abstract)
+            return execute_rabbit
+            # return execute_rabbit(abstract)
         elif abstract.program == "name_search":
-            return rabbit_name_search(abstract)
+            return rabbit_name_search
+            # return rabbit_name_search(abstract)
     elif abstract.county.engine == 'rattlesnake':
         if abstract.program in ["execute", "review", "download"]:
             # Need an additional prompt to handle early document downloads
             # BAD PRACTICE
             if abstract.program == 'download':
-                return execute_early_document_download(abstract)
+                return execute_early_document_download
+                # return execute_early_document_download(abstract)
             if abstract.program == 'execute':
                 add_download_type(abstract)
-            return execute_rattlesnake(abstract)
+            return execute_rattlesnake
+            # return execute_rattlesnake(abstract)
     else:
         print(f'"{abstract.county}" does not match available execution options, please review.')
         return False
@@ -62,5 +74,8 @@ def execute_program(abstract):
     abstractor = engine_switch(abstract)
     if abstractor is None:
         currently_unavailable(abstract)
+    elif abstractor is False:
+        quit()
     else:
-        abstractor(abstract)
+        browser = create_webdriver(abstract)
+        abstractor(browser, abstract)
