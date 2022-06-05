cae4f9d18901712b0724cfcde8f4d8b9f17244c9
Jack Hubert <jphubert93@gmail.com>
2022-06-04 18:30:32 -0600

    dropped the 'browser' parameter / arguments from the 'access_result_button' function

--- engines/octopus/open_document.py
+++ engines/octopus/open_document.py
@@ -14,7 +14,7 @@ def access_result_rows(browser, abstract, document):
     return locate_elements_by_class_name(browser, abstract.county.classes["Result Row"], "result rows", False, document)
 
 
-def access_result_button(browser, abstract, document, result_rows):
+def access_result_button(abstract, document, result_rows):
     result = result_rows[document.result_number]
     result_button = locate_element_by_class_name(result, abstract.county.buttons['Open Document'],
                                                  "result button", True, document)
@@ -27,7 +27,7 @@ def get_results(browser, abstract, document):
     result_links = []
     for result in result_rows:
         document.result_number = result_rows.index(result)
-        result_link = access_result_button(browser, abstract, document, result_rows)
+        result_link = access_result_button(abstract, document, result_rows)
         result_links.append(result_link)
     document.result_number = 0
     return result_links
@@ -38,7 +38,7 @@ def open_result(browser, abstract, document, result):
         javascript_script_execution(browser, result)
     else:
         result_rows = access_result_rows(browser, abstract, document)
-        result_link = access_result_button(browser, abstract, document, result_rows)
+        result_link = access_result_button(abstract, document, result_rows)
         javascript_script_execution(browser, result_link)
 
 
