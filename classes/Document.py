b716b4e979e605db0379ae93da973e0a7566d46e
Jack Hubert <jphubert93@gmail.com>
2022-06-04 21:25:05 -0600

    updated the syntax for 'extrapolate_document_value' for legal searches

--- classes/Document.py
+++ classes/Document.py
@@ -29,7 +29,7 @@ class Document:
 
     def document_value(self):
         if self.type in ["document_number", "name", "legal"]:
-            return str(self.value)
+            return [str(self.value)]
         elif self.type == "book_and_page":
             return [str(self.value["Book"]), str(self.value["Page"])]
         elif self.type == "volume_and_page":
@@ -48,7 +48,7 @@ class Document:
         elif self.type == "name":
             return f'search name "{self.value}"'
         elif self.type == "legal":
-            return f'search legal "{self.value}"'
+            return f'Township {self.value[1]} - Range {self.value[2]}, Section {self.value[0]}'
         else:
             print('Unable to identify document type while attempting to "extrapolate document value", please review.')
             return None
