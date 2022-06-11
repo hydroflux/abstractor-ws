7250ab8af5ff9173b75374bb7f9a563711b010d1
Jack Hubert <jphubert93@gmail.com>
2022-06-11 11:37:08 -0600

    adjusted the PDF Merger to work for octopus (need to create a more streamlined process

--- settings/pdf_merger.py
+++ settings/pdf_merger.py
@@ -12,7 +12,13 @@ def create_pdf_dictionary(target_directory, pdf_dictionary={}):
     for pdf in os.listdir(target_directory):
         if not pdf.endswith('.DS_Store'):
             # This is only going to work for rattlesnake for the time being--need to update naming syntax
-            pdf_name = f'{pdf[:15]}'
+            # pdf_name = f'{pdf[:15]}'  # Rattlesnake
+            # if pdf_name in pdf_dictionary:
+            #     pdf_dictionary[pdf_name].append(pdf[:-4])
+            # else:
+            #     pdf_dictionary[pdf_name] = [pdf[:-4]]
+            # octopus
+            pdf_name = f'{pdf[:13]}'
             if pdf_name in pdf_dictionary:
                 pdf_dictionary[pdf_name].append(pdf[:-4])
             else:
@@ -22,6 +28,7 @@ def create_pdf_dictionary(target_directory, pdf_dictionary={}):
 
 def merge_pdfs(target_directory, pdf_dictionary):
     for name, pdfs in pdf_dictionary.items():
+        print(name)
         merger = PdfFileMerger()
         [merger.append(PdfFileReader(f'{target_directory}/{pdf}.pdf', 'rb')) for pdf in pdfs]
         merger.write(f'{name}.pdf')
@@ -30,7 +37,9 @@ def merge_pdfs(target_directory, pdf_dictionary):
 
 def remove_original_files(target_directory):
     for file in os.listdir(target_directory):
-        if len(file) > 19 or file.endswith('.DS_Store'):
+        # if len(file) > 19 or file.endswith('.DS_Store'):  # Rattlesnake
+        print(len(file))
+        if len(file) > 17 or file.endswith('.DS_Store'):  # Octopus
             os.remove(file)
 
 
