27d03eb3459ceb06f7038a193e59da6aa13b9046
Jack Hubert <jphubert93@gmail.com>
2022-12-07 20:13:09 -0700

    updated functionality of the frame_handling script for buffalo in accordance with updates made to the generalized 'iframe handling' selenium script

--- engines/buffalo/frame_handling.py
+++ engines/buffalo/frame_handling.py
@@ -1,44 +1,44 @@
-from settings.county_variables.buffalo import (header_frame_name,
-                                               main_frame_name,
-                                               result_frame_name,
-                                               result_list_frame_name,
-                                               search_input_frame_name,
-                                               search_menu_frame_name)
-from settings.iframe_handling import (locate_iframe_by_name,
-                                      switch_to_default_content)
+from selenium_utilities.locators import locate_element_by_name
+from settings.iframe_handling import switch_to_default_content
 
 
-def switch_to_main_frame(browser):
+def switch_to_main_frame(browser, abstract):
     switch_to_default_content(browser)
-    main_frame = locate_iframe_by_name(browser, main_frame_name)
+    main_frame = locate_element_by_name(browser, abstract.county.iframes["Main"],
+                                        "main iframe")
     browser.switch_to.frame(main_frame)
 
 
-def switch_to_header_frame(browser):
-    switch_to_main_frame(browser)
-    header_frame = locate_iframe_by_name(browser, header_frame_name)
+def switch_to_header_frame(browser, abstract):
+    switch_to_main_frame(browser, abstract)
+    header_frame = locate_element_by_name(browser, abstract.county.iframes["Header"],
+                                          "header iframe")
     browser.switch_to.frame(header_frame)
 
 
-def switch_to_search_menu_frame(browser):
-    switch_to_main_frame(browser)
-    search_menu_frame = locate_iframe_by_name(browser, search_menu_frame_name)
+def switch_to_search_menu_frame(browser, abstract):
+    switch_to_main_frame(browser, abstract)
+    search_menu_frame = locate_element_by_name(browser, abstract.county.iframes["Search Menu"],
+                                               "search menu iframe")
     browser.switch_to.frame(search_menu_frame)
 
 
-def switch_to_search_input_frame(browser):
-    switch_to_search_menu_frame(browser)
-    search_input_frame = locate_iframe_by_name(browser, search_input_frame_name)
+def switch_to_search_input_frame(browser, abstract):
+    switch_to_search_menu_frame(browser, abstract)
+    search_input_frame = locate_element_by_name(browser, abstract.county.iframes["Search Input"],
+                                                "search input iframe")
     browser.switch_to.frame(search_input_frame)
 
 
-def switch_to_search_result_frame(browser):
-    switch_to_main_frame(browser)
-    result_frame = locate_iframe_by_name(browser, result_frame_name)
+def switch_to_search_result_frame(browser, abstract):
+    switch_to_main_frame(browser, abstract)
+    result_frame = locate_element_by_name(browser, abstract.county.iframes["Result"],
+                                          "result iframe")
     browser.switch_to_frame(result_frame)
 
 
-def switch_to_search_result_list_frame(browser):
-    switch_to_search_result_frame(browser)
-    result_list_frame = locate_iframe_by_name(browser, result_list_frame_name)
+def switch_to_search_result_list_frame(browser, abstract):
+    switch_to_search_result_frame(browser, abstract)
+    result_list_frame = locate_element_by_name(browser, abstract.county.iframes["Result List"],
+                                               "result list iframe")
     browser.switch_to_frame(result_list_frame)
