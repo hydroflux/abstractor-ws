f362296289e78e927e7ef0705db706b1018752d5
Jack Hubert <jphubert93@gmail.com>
2022-12-08 16:32:06 -0700

    added a 'check_for_captcha' validation function

--- engines/buffalo/validation.py
+++ engines/buffalo/validation.py
@@ -1,4 +1,5 @@
-from engines.buffalo.frame_handling import switch_to_header_frame
+from engines.buffalo.frame_handling import (switch_to_captcha_frame,
+                                            switch_to_header_frame)
 
 from selenium_utilities.locators import locate_element_by_tag_name
 
@@ -10,10 +11,16 @@ def header_validation(browser, abstract, validation_text):
     return header_text_element.text.startswith(validation_text)
 
 
+def check_for_captcha(browser, abstract):
+    while switch_to_captcha_frame(browser, abstract):
+        input("Encountered a captcha while processing abstract, please address and press enter to continue...")
+
+
 def page_is_loaded(browser, abstract, validation_text):
     if not header_validation(browser, abstract, validation_text):
         print(f'Browser indicates that it has not properly loaded '
               f'"{validation_text}", please review webdriver before continuing...')
         input()
     else:
+        check_for_captcha(browser, abstract)
         return True
