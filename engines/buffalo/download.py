3af75efa338e89dc4402a98fc1c01834e0d4cfc6
Jack Hubert <jphubert93@gmail.com>
2022-12-08 16:28:02 -0700

    created a 'download' script for buffalo

---
+++ engines/buffalo/download.py
@@ -0,0 +1,26 @@
+from engines.buffalo.frame_handling import (switch_to_download_frame,
+                                            switch_to_download_submenu_frame)
+
+from project_management.timers import micro_nap
+
+from selenium_utilities.locators import locate_element
+
+
+def open_download_submenu(browser, abstract, document):
+    switch_to_download_submenu_frame(browser, abstract)
+    download_submenu_button = locate_element(browser, "id", abstract.county.buttons["Download Submenu"],
+                                             "download submenu button", True, document)
+    download_submenu_button.click()
+
+
+def download_document(browser, abstract, document):
+    switch_to_download_frame(browser, abstract)
+    download_button = locate_element(browser, "xpath", abstract.county.buttons["Download"],
+                                     "download button", True, document)
+    download_button.click()
+
+
+def execute_download(browser, abstract, document):
+    open_download_submenu(browser, abstract, document)
+    micro_nap()
+    download_document(browser, abstract, document)
