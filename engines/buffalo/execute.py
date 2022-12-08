1e0f062a11ae5eded39e9332fa78c9bd71fb2831
Jack Hubert <jphubert93@gmail.com>
2022-12-07 20:11:54 -0700

    updated and cleaned up the buffalo execution script => WIP

--- engines/buffalo/execute.py
+++ engines/buffalo/execute.py
@@ -1,55 +1,24 @@
 #!/usr/bin/python3
-from settings.objects.abstract_dataframe import abstract_dictionary as dataframe
-from settings.invalid import record_invalid_search
-from settings.driver import create_webdriver
-from project_management.export import export_document
-from settings.dataframe_management import (bundle_project, check_length,
-                                      document_found,
-                                      extrapolate_document_value,
-                                      no_document_found)
-from settings.general_functions import start_timer
+from serializers.executor import close_program, search_documents_from_list
 
-from engines.buffalo.login import account_login
+from engines.buffalo.login import login
 from engines.buffalo.logout import logout
 from engines.buffalo.open_document import open_document
 from engines.buffalo.search import search
 
-
-def record_single_document(browser, county, target_directory, document_list, document, start_time):
-    pass
-    document_found(start_time, document_list, document)
+# open_document => browser, abstract, document
+# record => browser, abstract, document
 
 
-def handle_search_results(browser, county, target_directory, document_list, document, start_time):
-    if document.number_results == 1:
-        record_single_document(browser, county, target_directory, document_list, document, start_time)
-    elif document.number_results > 1:
-        print(f'Browser located multiple results for '
-              f'{extrapolate_document_value(document)}; '
-              f'No currently process built to handle multiple documents, please review.')
-        input()
-
-
-# Identical to crocodile search_documents_from_list
-def search_documents_from_list(browser, county, target_directory, document_list):
-    for document in document_list:
-        start_time = start_timer()
-        search(browser, document)
-        if open_document(browser, document):
-            handle_search_results(browser, county, target_directory, document_list, document, start_time)
-        else:
-            record_invalid_search(dataframe, document)
-            no_document_found(start_time, document_list, document)
-        check_length(dataframe)
-    return dataframe
-
-
-# Identical to crocodile execute_program
-def execute_program(county, target_directory, document_list, file_name):
-    browser = create_webdriver(target_directory, False)
-    account_login(browser)
-    dataframe = search_documents_from_list(browser, county, target_directory, document_list)
-    logout(browser)
-    abstraction = export_document(county, target_directory, file_name, dataframe)
-    bundle_project(target_directory, abstraction)
-    browser.close()
+def execute_program(browser, abstract):
+    login(browser, abstract)
+    search_documents_from_list(
+        browser,
+        abstract,
+        search,
+        open_document,
+        # record,
+        # execute_download
+        # next_result
+    )
+    close_program(browser, abstract, logout)
