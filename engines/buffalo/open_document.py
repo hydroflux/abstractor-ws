47409bb093167e56125585957a985a255406b392
Jack Hubert <jphubert93@gmail.com>
2022-12-13 16:29:09 -0700

    further refactoring of the open_document script for simplicity

--- engines/buffalo/open_document.py
+++ engines/buffalo/open_document.py
@@ -25,14 +25,6 @@ def count_results(browser, abstract, document):
               f'{document.extrapolate_value()}.')
 
 
-# def check_search_results(browser, abstract, document):
-#     count_results(browser, abstract, document)
-#     if document.number_results > 1:
-#         input(f'Returned {document.number_results} results while searching for '
-#               f'{document.extrapolate_value()}... Please review application logic for processing '
-#               f'{document.number_results} results.')
-
-
 def get_first_result_link(browser, abstract, document):
     switch_to_search_result_list_frame(browser, abstract)
     return locate_element(browser, "id", abstract.county.ids["First Result Link"],
@@ -54,28 +46,9 @@ def verify_first_document_search_result(browser, abstract, document):
         if first_result == eight_character_padding(document.value):
             return True
     elif document.type == "book_and_page":
-        book = four_character_padding(str(document.value["Book"]))
-        page = four_character_padding(str(document.value["Page"]))
-        if first_result == f'{book} {page}':
+        book, page = document.document_value()
+        if first_result == f'{four_character_padding(book)} {four_character_padding(page)}':
             return True
-
-
-# def open_document_number(browser, abstract, document):
-#     if verify_first_document_search_result(browser, abstract, document):
-#         get_first_result_link(browser, abstract, document).click()
-#     else:
-#         print(f'First search result located does not match the searched document '
-#               f'{document.extrapolate_value()}, please review')
-#         input()
-
-
-# def process_open_document(browser, abstract, document):
-#     if document.type == "document_number":
-#         open_document_number(browser, abstract, document)
-#     else:
-#         print(f'Document type {document.type} not currently available, '
-#               f'please review entry...')
-#         input()
 
 
 def process_open_document(browser, abstract, document):
