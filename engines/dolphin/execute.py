6a65eff7f03c2d51db8aa521110aa831b63918c0
Jack Hubert <jphubert93@gmail.com>
2023-02-07 17:23:59 -0700

    created an 'execute' script for dolphin

---
+++ engines/dolphin/execute.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from engines.dolphin.collect import collect
+from engines.dolphin.download import execute_download
+from engines.dolphin.login import login
+from engines.dolphin.logout import logout
+from engines.dolphin.open_document import open_document
+from engines.dolphin.record import record
+from engines.dolphin.search import search
+
+from serializers.executor import close_program, search_documents_from_list
+
+
+def check_for_document_list(browser, abstract):
+    if abstract.program in ['name_search', 'legal']:
+        search(browser, abstract)
+        collect(browser, abstract)
+
+
+def execute_legal_search(browser, abstract):
+    login(browser, abstract)
+    check_for_document_list(browser, abstract)
+    search_documents_from_list(
+        browser,
+        abstract,
+        search,
+        open_document,
+        record,
+        execute_download
+    )
+    close_program(browser, abstract, logout)
