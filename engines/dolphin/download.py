c8167e36c558ba135d2cfc996e7ed1031e52f8e0
Jack Hubert <jphubert93@gmail.com>
2023-02-07 17:23:48 -0700

    created a download script for dolphin

---
+++ engines/dolphin/download.py
@@ -0,0 +1,17 @@
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import locate_element_by_id
+
+from engines.dolphin.validation import check_for_document_image
+
+# Exact same as "octopus"
+
+
+def execute_download(browser, abstract, document):
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Download Prompt"],
+                 "download prompt button", document)
+    if check_for_document_image(browser, abstract, document):
+        click_button(browser, locate_element_by_id, abstract.county.buttons["Download"],
+                     "download button", document)
+    else:
+        click_button(browser, locate_element_by_id, abstract.county.buttons["Cancel Download"],
+                     "cancel download button", document)
