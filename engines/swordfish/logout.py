7baaaca34f5c4e5198ef08a4da1b0a568254bb12
Jack Hubert <jphubert93@gmail.com>
2023-02-16 16:14:08 -0700

    created a basic 'logout' script for swordfish

---
+++ engines/swordfish/logout.py
@@ -0,0 +1,16 @@
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import (locate_element_by_id,
+                                         locate_elements_by_class_name)
+
+# Exact same functionality as "octopus" & "dolphin"
+
+
+def open_logout_dropdown(browser, abstract):
+    dropdown_buttons = locate_elements_by_class_name(browser, abstract.county.buttons["Logout Dropdown"],
+                                                     "logout dropdown", True)
+    dropdown_buttons[1].click()
+
+
+def logout(browser, abstract):
+    open_logout_dropdown(browser, abstract)
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Logout"], "logout button", True)
