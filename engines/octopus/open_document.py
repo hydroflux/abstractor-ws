84ed17b7188d064f35bad5dd83156cbbe087e669
Jack Hubert <jphubert93@gmail.com>
2023-02-16 18:04:07 -0700

    updated 'open_document' procedure for dolphin, octopus, & swordfish to check again for document results before attempting to open document

--- engines/octopus/open_document.py
+++ engines/octopus/open_document.py
@@ -1,11 +1,12 @@
 from engines.octopus.collect import collect
 from settings.general_functions import javascript_script_execution
 
-# Exact same as "dolphin" except for import
+# Exact same as "dolphin" & "swordfish" except for import
 
 
 def open_document(browser, abstract, document):
-    if not document.description_link or document.result_number > 0:
-        collect(browser, abstract, document)
-    javascript_script_execution(browser, document.description_link)
-    return True
+    if document.result_number != 0:
+        if not document.description_link or document.result_number > 0:
+            collect(browser, abstract, document)
+        javascript_script_execution(browser, document.description_link)
+        return True
--- engines/swordfish/open_document.py
+++ engines/swordfish/open_document.py
@@ -5,7 +5,8 @@ from settings.general_functions import javascript_script_execution
 
 
 def open_document(browser, abstract, document):
-    if not document.description_link or document.result_number > 0:
-        collect(browser, abstract, document)
-    javascript_script_execution(browser, document.description_link)
-    return True
+    if document.result_number != 0:
+        if not document.description_link or document.result_number > 0:
+            collect(browser, abstract, document)
+        javascript_script_execution(browser, document.description_link)
+        return True
