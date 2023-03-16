51100ec3a99ebf106cd2a94bd26d0f8df28b99eb
Jack Hubert <jphubert93@gmail.com>
2023-03-15 18:09:32 -0600

    created a simple 'error_handling' script for tiger

---
+++ engines/tiger/error_handling.py
@@ -0,0 +1,6 @@
+# Used in tiger 'search' function
+def check_for_browser_error(browser):
+    if browser.title == "Error":
+        print("Browser encountered an error during the search, refreshing the page to attempt to fix the problem.")
+        # Review after hitting this error again, browser needs to still be logged in during error to see if this works
+        browser.refresh()
