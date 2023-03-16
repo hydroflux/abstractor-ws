37bc921245fd53acf53a490ca5ef03214d49190b
Jack Hubert <jphubert93@gmail.com>
2023-03-15 18:05:50 -0600

    created a 'disclaimer' script for the tiger engine

---
+++ engines/tiger/disclaimer.py
@@ -0,0 +1,25 @@
+from project_management.timers import naptime
+
+from selenium_utilities.inputs import click_button
+from selenium_utilities.locators import locate_element_by_id
+
+from settings.general_functions import javascript_script_execution
+
+
+def handle_disclaimer(browser, abstract):
+    javascript_script_execution(
+        browser,
+        abstract.county.scripts["Open Search"]
+    )
+    disclaimer = locate_element_by_id(browser,
+                                      abstract.county.ids["Disclaimer"],
+                                      "disclaimer"
+                                      )
+    if disclaimer.get_attribute('class') == abstract.county.classes["Disclaimer Active"]:
+        click_button(
+            browser,
+            locate_element_by_id,
+            abstract.county.buttons["Disclaimer"],
+            "disclaimer button"
+        )
+        naptime()
