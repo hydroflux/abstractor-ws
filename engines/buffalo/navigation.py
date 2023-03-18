4be6fff4d82c2577df14114753d3e27f4a020086
Jack Hubert <jphubert93@gmail.com>
2023-03-18 17:50:07 -0600

    updated the buffalo 'navigation' script to handle for bad element clicks

--- engines/buffalo/navigation.py
+++ engines/buffalo/navigation.py
@@ -1,6 +1,7 @@
-from engines.buffalo.frame_handling import switch_to_document_frame
-
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium_utilities.locators import locate_element
+
+from engines.buffalo.frame_handling import switch_to_main_frame, switch_to_document_frame
 
 
 def get_next_result_button(browser, abstract, document):
@@ -9,7 +10,25 @@ def get_next_result_button(browser, abstract, document):
                           "next result button", True, document)
 
 
+def clear_download_alert(browser, abstract, document):
+    switch_to_main_frame(browser, abstract)
+    clear_download_alert_button = locate_element(browser, "class", abstract.county.buttons["Download Alert"],
+                                                 "download alert button", True, document, True)
+    clear_download_alert_button.click()
+
+
+def execute_next_result_click(browser, abstract, document):
+    try:
+        next_result_button = get_next_result_button(browser, abstract, document)
+        # handle_click_next_result_button(browser, abstract, document, next_result_button)
+        next_result_button.click()
+        return True
+    except ElementClickInterceptedException:
+        print("Encountered an ElementClickInterceptedException, trying again...")
+        clear_download_alert(browser, abstract, document)
+        return False
+
+
 def next_result(browser, abstract, document):
-    next_result_button = get_next_result_button(browser, abstract, document)
-    # handle_click_next_result_button(browser, abstract, document, next_result_button)
-    next_result_button.click()
+    while not execute_next_result_click(browser, abstract, document):
+        execute_next_result_click(browser, abstract, document)
