121f1c18da64fb0a34ed7c925ea7c3434c0efc18
Jack Hubert <jphubert93@gmail.com>
2023-03-24 00:02:45 -0600

    added additional grantor & grantee options for tiger record

--- engines/tiger/record.py
+++ engines/tiger/record.py
@@ -123,6 +123,8 @@ def record_grantor(abstract, rows):
                             grantor = check_rows(abstract, rows, abstract.county.titles["Row Titles"]["sixth_alt_grantor"])
                             if grantor == not_applicable:
                                 grantor = check_rows(abstract, rows, abstract.county.titles["Row Titles"]["seventh_alt_grantor"])
+                                if grantor == not_applicable:
+                                    grantor = check_rows(abstract, rows, abstract.county.titles["Row Titles"]["eighth_alt_grantor"])
     record_value(abstract, 'grantor', grantor.title())
 
 
@@ -144,6 +146,8 @@ def record_grantee(abstract, rows):
                                 grantee = check_rows(abstract, rows, abstract.county.titles["Row Titles"]["seventh_alt_grantee"])
                                 if grantee == not_applicable:
                                     grantee = check_rows(abstract, rows, abstract.county.titles["Row Titles"]["eighth_alt_grantee"])
+                                    if grantee == not_applicable:
+                                        grantee = check_rows(abstract, rows, abstract.county.titles["Row Titles"]["nineth_alt_grantee"])
     record_value(abstract, 'grantee', grantee.title())
 
 
