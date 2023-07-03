42a3f8cd45a6c4f3c027010b2ee85289f1d9bc36
Jack Hubert <jphubert93@gmail.com>
2023-07-03 15:14:23 -0600

    added a function to wait for search results page to load prior to attemption to open the document link

--- engines/swordfish/open_document.py
+++ engines/swordfish/open_document.py
@@ -1,11 +1,20 @@
 from engines.swordfish.collect import collect
+from selenium_utilities.locators import locate_element_by_tag_name
 from settings.general_functions import javascript_script_execution
 
 # Exact same as "octopus" & "dolphin" except for import
 
 
+def search_results_page_loaded(browser, abstract, document):
+    page_header = locate_element_by_tag_name(browser, "h2", "page header", False, document)
+    if page_header.text.startswith("Results"):
+        return True
+
+
 def open_document(browser, abstract, document):
-    if not document.description_link or document.result_number > 0:
-        collect(browser, abstract, document)
-    javascript_script_execution(browser, document.description_link)
-    return True
+    if search_results_page_loaded(browser, abstract, document):
+        if not document.description_link or document.result_number > 0:
+            if not collect(browser, abstract, document):
+                return False
+        javascript_script_execution(browser, document.description_link)
+        return True
