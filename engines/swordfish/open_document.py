878d2cac1c0e8fd84139d72691c76cd6ff119fd2
Jack Hubert <jphubert93@gmail.com>
2023-07-03 15:18:29 -0600

    extrapolated the logic from 'open_document' following the search results loading into a single function titled 'open_search_result' rather than following the conditional chain in the same function

--- engines/swordfish/open_document.py
+++ engines/swordfish/open_document.py
@@ -11,10 +11,15 @@ def search_results_page_loaded(browser, abstract, document):
         return True
 
 
+def open_search_result(browser, abstract, document):
+    if not document.description_link or document.result_number > 0:
+        if not collect(browser, abstract, document):
+            return False
+    javascript_script_execution(browser, document.description_link)
+    return True
+    
+    
+
 def open_document(browser, abstract, document):
     if search_results_page_loaded(browser, abstract, document):
-        if not document.description_link or document.result_number > 0:
-            if not collect(browser, abstract, document):
-                return False
-        javascript_script_execution(browser, document.description_link)
-        return True
+        open_search_result(browser, abstract, document)
