89e0a1c71e15d4db35c3740d1b29d8a188074fb6
Jack Hubert <jphubert93@gmail.com>
2023-07-12 18:48:47 -0600

    added a 'book_and_page' search path to the swordfish search script using a selector element

--- engines/swordfish/search.py
+++ engines/swordfish/search.py
@@ -1,7 +1,9 @@
+from selenium.webdriver.support.ui import Select
 from selenium_utilities.inputs import click_button, enter_input_value
-from selenium_utilities.locators import (locate_element_by_class_name,
+from selenium_utilities.locators import (locate_element, locate_element_by_class_name,
                                          locate_element_by_id)
 from selenium_utilities.open import open_url
+from selenium.common.exceptions import NoSuchElementException
 
 # Exact same functionality as "octopus" & "dolphin"
 
@@ -9,6 +11,32 @@ from selenium_utilities.open import open_url
 def handle_continuing_collection_search(browser, abstract, document):
     if abstract.document_list.index(document) > 0:
         browser.back()
+
+
+def select_book_value(browser, abstract, document):
+    book = document.document_value()[0].zfill(4)
+    try:
+        book_selector = Select(
+            locate_element(browser, "id", abstract.county.inputs["Book"], "book selector",
+                        clickable=True, document=document)
+        )
+        book_selector.select_by_visible_text(str(book))
+        return True
+    except NoSuchElementException:
+        return False
+    # enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Book"],
+    #                     "book input", book, document)
+
+
+def select_page_value(browser, abstract, document):
+    page = document.document_value()[1]
+    # page_selector = Select(
+    #     locate_element(browser, "id", abstract.county.inputs["Page"], "page selector",
+    #                    clickable=True, document=document)
+    # )
+    # page_selector.select_by_visible_text(str(page))
+    enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Page"],
+                      "page input", page, document)
 
 
 def handle_search_values(browser, abstract, document):
@@ -19,16 +47,16 @@ def handle_search_values(browser, abstract, document):
                           "township input", f'T{abstract.legal[1]}N')
         enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Range"],
                           "range input", f'R{abstract.legal[2]}W')
+        return True
     else:
-        value = document.document_value()
         if document.type == "document_number":
             enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Reception Number"],
-                              "reception number input", value, document)
-        # elif document.type == "book_and_page":
-        #     enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Book"],
-        #                       "book input", value[0], document)
-        #     enter_input_value(browser, locate_element_by_id, abstract.county.inputs["Page"],
-        #                       "page input", value[1], document)
+                              "reception number input", document.document_value(), document)
+            return True
+        elif document.type == "book_and_page":
+            if select_book_value(browser, abstract, document):
+                select_page_value(browser, abstract, document)
+                return True
         else:
             print(f'Abstractor path has not yet been developed to "handle search values" for document type "\
                 {document.type}", please review...')
@@ -36,9 +64,9 @@ def handle_search_values(browser, abstract, document):
 
 
 def execute_search(browser, abstract, document):
-    handle_search_values(browser, abstract, document)
-    click_button(browser, locate_element_by_class_name, abstract.county.buttons["Search"],
-                 "execute search button", document)
+    if handle_search_values(browser, abstract, document):
+        click_button(browser, locate_element_by_class_name, abstract.county.buttons["Search"],
+                    "execute search button", document)
 
 
 def search(browser, abstract, document=None):
