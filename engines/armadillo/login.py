31c58ca6be02e5cd28c772a23a4063a3b3e1497a
Jack Hubert <jphubert93@gmail.com>
2023-10-03 18:41:11 -0600

    created a 'login' script file based on the eagle & jaguar scripts

--- engines/armadillo/login.py
+++ engines/armadillo/login.py
@@ -1,21 +1,80 @@
+from selenium.common.exceptions import (StaleElementReferenceException,
+                                        TimeoutException)
+
+from project_management.timers import micro_nap
+
 from selenium_utilities.inputs import click_button, enter_input_value
-from selenium_utilities.locators import locate_element_by_name as locate_input
+from selenium_utilities.locators import (locate_element_by_class_name,
+                                         locate_element_by_id)
 from selenium_utilities.open import open_url
 
-from settings.county_variables.armadillo import credentials, login_title, login_url
-
-from engines.armadillo.validation import verify_login
+from engines.armadillo.disclaimer import check_for_disclaimer
 
 
-def enter_credentials(browser):
-    enter_input_value(browser, locate_input, credentials[0],  # Submit Username
-                      "username input", credentials[1])
-    enter_input_value(browser, locate_input, credentials[2],  # Submit Password
-                      "password input", credentials[3])
-    click_button(browser, locate_input, credentials[4], 'submit')  # Click Login Button
+def open_site(browser, abstract):
+    open_url(browser, abstract.county.urls["Home Page"],
+             abstract.county.titles["Home Page"], "county site")
+    input("Press enter to login...")
 
 
-def account_login(browser):
-    open_url(browser, login_url, login_title, "county site")  # Open County Site
-    enter_credentials(browser)  # Enter Login Information
-    verify_login(browser)  # Verify Login
+def enter_credentials(browser, abstract):
+    enter_input_value(browser, locate_element_by_id, abstract.county.ids["Username"],
+                      "username input", abstract.county.credentials[0])
+    enter_input_value(browser, locate_element_by_id, abstract.county.ids["Password"],
+                      "password input", abstract.county.credentials[1])
+    click_button(browser, locate_element_by_id, abstract.county.buttons["Login"], "login button")
+
+
+def read_login_message(browser, abstract):
+    try:
+        login_message = locate_element_by_id(browser, abstract.county.ids["Welcome"],
+                                             "login message", True)
+        return login_message.text
+    except StaleElementReferenceException:
+        print('Encountered StaleElementReferenceException '
+              'attempting to read login message, trying again.')
+
+
+def confirm_login(browser, abstract):
+    while read_login_message(browser, abstract) != abstract.county.credentials[2]:
+        micro_nap()
+
+
+def log_back_in(browser, abstract):
+    try:
+        click_button(browser, locate_element_by_class_name, abstract.county.classes["Login Prompt"],
+                     abstract.county.buttons["Login"], "login button prompt")
+        enter_credentials(browser, abstract)
+        confirm_login(browser, abstract)
+    except TimeoutException:
+        print("Browser timed out while trying to log back in after logout.")
+
+
+# def check_login_status(browser, abstract):
+#     check_for_disclaimer(browser, abstract)
+#     while browser.current_url == abstract.county.urls["Log Out Redirect"]:
+#         print('Browser redirected to login screen, checking login status & returning to search.')
+#         if read_login_message(browser, abstract) != abstract.county.credentials[2]:
+#             print('Browser logged out, attempting to log back in.')
+#             log_back_in(browser, abstract)
+#         browser.get(abstract.county.urls["Fallback Search"])
+
+
+def execute_login_process(browser, abstract):
+    open_site(browser, abstract)
+    check_for_disclaimer(browser, abstract)
+    click_button(browser, locate_element_by_class_name, abstract.county.classes["Login Prompt"],
+                 abstract.county.buttons["Login"], "login button prompt")
+    enter_credentials(browser, abstract)
+    confirm_login(browser, abstract)
+    return True
+
+
+def login(browser, abstract):
+    print("\nWebdriver initialized, attempting to login...")
+    if not execute_login_process(browser, abstract):
+        print("Login sequence failed, attempting again before closing out.")
+        if not execute_login_process(browser, abstract):
+            print("Browser failed to properly login, please review & try again later.")
+            browser.quit()
+            exit()
