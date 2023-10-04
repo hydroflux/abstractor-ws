2cdd30ec645e9a0e2aea1e6efd8c30ea5b1f2b0d
Jack Hubert <jphubert93@gmail.com>
2023-10-03 18:43:15 -0600

    created a new 'search' script file based on the eagle & jaguar scripts for the new armadillo platform

--- engines/armadillo/search.py
+++ engines/armadillo/search.py
@@ -1,51 +1,51 @@
-from selenium_utilities.inputs import clear_input, enter_input_value
+from project_management.timers import naptime
+
+from selenium_utilities.inputs import (clear_input, click_button,
+                                       enter_input_value)
 from selenium_utilities.locators import locate_element_by_id as locate_input
 from selenium_utilities.open import open_url
 
-from settings.county_variables.armadillo import (
-    execute_document_search_script, search_title, search_url)
-from settings.general_functions import javascript_script_execution
-
-# from armadillo.validation import verify_document_search_page_loaded
+from engines.armadillo.error_handling import check_for_error
+# from engines.armadillo.login import check_login_status
 
 
-def clear_search(browser, document):
-    clear_input(browser, locate_input, document.input_attributes["Reception Number"],
-                "reception number input", document)
-    clear_input(browser, locate_input, document.input_attributes["Volume"],
-                "volume input", document)
-    clear_input(browser, locate_input, document.input_attributes["Page"],
-                "page input", document)
+def clear_search(browser, abstract, document):
+    for id in abstract.county.inputs:
+        clear_input(browser, locate_input, abstract.county.inputs[id],
+                    f'{id} Input', document)
 
 
-def handle_document_value_numbers(browser, document):
+def handle_document_value_numbers(browser, abstract, document):
     value = document.document_value()
-    if document.type == 'document_number':
-        enter_input_value(browser, locate_input, document.input_attributes["Reception Number"],
+    if document.type == "document_number":
+        enter_input_value(browser, locate_input, abstract.county.inputs["Reception Number"],
                           "reception number input", value, document)
-    elif document.type == 'volume_and_page':
-        enter_input_value(browser, locate_input, document.input_attributes["Volume"],
+    elif document.type == "book_and_page":
+        enter_input_value(browser, locate_input, abstract.county.inputs["Book"],
+                          "book input", value[0], document)
+        enter_input_value(browser, locate_input, abstract.county.inputs["Volume"],
                           "volume input", value[0], document)
-        enter_input_value(browser, locate_input, document.input_attributes["Page"],
+        enter_input_value(browser, locate_input, abstract.county.inputs["Page"],
                           "page input", value[1], document)
-    else:
-        print(f'Unable to search document type "{document.type}", '
-              f'a new search path needs to be developed in order to continue.\n')
-        print("Please press enter after reviewing the search parameters...")
-        input()
 
 
-def execute_search(browser):
-    javascript_script_execution(browser, execute_document_search_script)
+def execute_search(browser, abstract, document):
+    handle_document_value_numbers(browser, abstract, document)
+    click_button(browser, locate_input, abstract.county.buttons["Submit Search"],
+                 "execute search button", document)
 
 
-def document_search(browser, document):
-    handle_document_value_numbers(browser, document)
-    execute_search(browser)
+def open_search(browser, abstract, document):
+    print(f'Searching for document located at {document.extrapolate_value()}')
+    open_url(browser, abstract.county.urls["Search Page"],
+             abstract.county.titles["Search Page"], "document search page")
+    # check_login_status(browser, abstract)
 
 
-def search(browser, document):
-    open_url(browser, search_url, search_title, 'document search page', document)
-    # verify_document_search_page_loaded(browser, document)
-    clear_search(browser, document)
-    document_search(browser, document)
+def search(browser, abstract, document):
+    open_search(browser, abstract, document)
+    if not check_for_error(browser, abstract, document, 'search'):
+        clear_search(browser, abstract, document)
+        naptime()  # Consider testing without this nap to see if necessary
+        execute_search(browser, abstract, document)
+        # naptime()
