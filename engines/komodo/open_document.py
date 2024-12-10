5fd4c22ca1fae6748b5589ba2e59c0ee5cae7ee1
Jack Hubert <jphubert93@gmail.com>
2024-12-09 17:34:43 -0700

    created a basic 'open_document' script for komodo -- specifically only used for 'name_search' programs at the moment

---
+++ engines/komodo/open_document.py
@@ -0,0 +1,9 @@
+
+
+from selenium_utilities.open import open_url
+
+
+def open_document(browser, abstract, document):
+    if abstract.program == ["name_search"]:
+        open_url(browser, document.description_link, abstract.county.titles["Search Result Page"],
+                 f"reception number {document.reception_number} at {document.description_link}")
