3510cda2bd8327108b38146ac0ec47b80b39f1d5
Jack Hubert <jphubert93@gmail.com>
2024-12-10 13:26:20 -0700

    corrected syntax issue with open document

--- engines/komodo/open_document.py
+++ engines/komodo/open_document.py
@@ -1,9 +1,7 @@
-
-
 from selenium_utilities.open import open_url
 
 
 def open_document(browser, abstract, document):
-    if abstract.program == ["name_search"]:
+    if abstract.program == "name_search":
         open_url(browser, document.description_link, abstract.county.titles["Search Result Page"],
                  f"reception number {document.reception_number} at {document.description_link}")
