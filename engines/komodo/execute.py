fe2b7a8bc79c4d946574169997ba79a7de7c8d97
Jack Hubert <jphubert93@gmail.com>
2024-12-24 01:40:02 -0700

    moved around the functioning of 'collect' and added 'next_result' to the execute script

--- engines/komodo/execute.py
+++ engines/komodo/execute.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from engines.komodo.collect import collect
 from engines.komodo.download import execute_download
 from engines.komodo.login import login
 from engines.komodo.logout import logout
-from engines.komodo.open_document import open_document
+from engines.komodo.open_document import next_result, open_document
 from engines.komodo.record import record
 from engines.komodo.search import search
 # from engines.komodo.navigation import next_result
@@ -16,7 +15,6 @@ from serializers.executor import close_program, search_documents_from_list
 def check_for_document_list(browser, abstract):
     if abstract.program in ['name_search', 'legal']:
         search(browser, abstract)
-        collect(browser, abstract)
 
 
 def execute_program(browser, abstract):
@@ -29,6 +27,6 @@ def execute_program(browser, abstract):
         open_document,
         record,
         execute_download,
-        # next_result
+        next_result
     )
     close_program(browser, abstract, logout)
